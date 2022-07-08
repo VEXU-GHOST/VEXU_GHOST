9abc5b85040192647cac4402f4a1afa4b43d053b
Maxx Wilson - Desktop <JesseMaxxWilson@utexas.edu>
2022-07-08 13:26:01 -0500

    move deprecated gazebo launch

---
+++ swerve_drive/launch/gazebo/upload_swerve.launch.py
@@ -0,0 +1,40 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+
+    swerve_drive_share_dir = get_package_share_directory('swerve_drive')
+    
+    urdf_path = "/home/maxx/HCR/PyPnC/robot_model/atlas/atlas.urdf" #os.path.join(swerve_drive_share_dir, "urdf", "swerve.urdf")
+
+    doc = xacro.process(urdf_path)
+    
+    # spawn_entity_args = ("-x 0.0 -y 0.0 -z 0.0 -R 0.0 -P 0.0 -Y 0.0 -entity swerve -topic robot_description").split()
+    spawn_entity_args = ("-topic robot_description").split()
+    
+    with open(os.path.join(swerve_drive_share_dir, "urdf", "robot_description.urdf"), 'w') as file:
+        file.write(doc)
+
+    return LaunchDescription([
+        Node(
+            package = "gazebo_ros",
+            executable = "spawn_entity.py",
+            output='screen',
+            arguments=spawn_entity_args
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': False}, {"robot_description": doc}],
+        ),
+        
+    ])
