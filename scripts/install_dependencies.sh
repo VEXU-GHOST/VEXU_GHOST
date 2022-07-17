33de638493b453a3779f4ec3b7c722c9d8db34ba
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-07-17 18:00:08 -0500

    removes pybullet install addes gazebo11 install through ros pkgs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt-get install -y python3-colcon-common-extensions ros-foxy-xacro
+sudo apt-get install -y python3-colcon-common-extensions ros-foxy-xacro gazebo11 ros-foxy-gazebo-ros
 sudo apt-get install -y ros-foxy-joint-state-publisher ros-foxy-joint-state-publisher-gui ros-foxy-rqt-tf-tree
-sudo apt-get install -y python3-pip 
-python3 -m pip install pybullet
+sudo apt-get install -y python3-pip
