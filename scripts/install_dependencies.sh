d695da1022dae9483445d62f0142c603b502cfec
Xander Wilson <xanderwilson2014@gmail.com>
2022-07-17 18:58:45 -0500

    Added Gazebo to install_dependencies.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install -y python3-colcon-common-extensions ros-foxy-xacro gazebo11 ros-foxy-gazebo-ros
+sudo apt-get install -y python3-colcon-common-extensions ros-foxy-xacro ros-foxy-gazebo-ros
 sudo apt-get install -y ros-foxy-joint-state-publisher ros-foxy-joint-state-publisher-gui ros-foxy-rqt-tf-tree
 sudo apt-get install -y python3-pip
