8b33f0ea96ce0441c10d7bb2b26320623334d092
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-10-26 16:03:04 -0500

    modifies build script to add sim only on x86

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# Get processor architecture to determine if we should build simulator or not (not on robot hardware)
+arch=$(uname -p)
+
+# Assumes repository is in base directory
 cd ~/VEXU_GHOST
-colcon build
+
+# Build ignores simulator packages on embedded devices
+if [ "$arch" == 'x86_64' ];
+then 
+    colcon build
+fi
+
+if [ "$arch" == 'aarch65' ];
+then 
+    colcon build --packages-select ghost_ros
+fi
+
 source install/setup.bash
 
+# Process URDFs from Xacro and add to Share
 GHOST_ROS_SHARE_DIR="$PWD/install/ghost_ros/share/ghost_ros"
 URDF_PATH="${GHOST_ROS_SHARE_DIR}/urdf/ghost1.urdf"
 
@@ -17,21 +33,25 @@ else
     echo "URDF written to" $URDF_PATH
 fi
 
-GHOST_SIM_SHARE_DIR="$PWD/install/ghost_sim/share/ghost_sim"
-URDF_SIM_VOLTAGE_PATH="${GHOST_SIM_SHARE_DIR}/urdf/ghost1_sim_voltage.urdf"
-URDF_SIM_PID_PATH="${GHOST_SIM_SHARE_DIR}/urdf/ghost1_sim_pid.urdf"
+# Processes URDFs for simulation
+if [ "$arch" == 'x86_64' ];
+then 
+    GHOST_SIM_SHARE_DIR="$PWD/install/ghost_sim/share/ghost_sim"
+    URDF_SIM_VOLTAGE_PATH="${GHOST_SIM_SHARE_DIR}/urdf/ghost1_sim_voltage.urdf"
+    URDF_SIM_PID_PATH="${GHOST_SIM_SHARE_DIR}/urdf/ghost1_sim_pid.urdf"
 
-if [ ! -d $GHOST_SIM_SHARE_DIR ];
-then
-    touch $URDF_SIM_VOLTAGE_PATH
-    touch $URDF_SIM_PID_PATH
-else
-    echo
-    echo "Generating URDF"
-    xacro ghost_sim/urdf/ghost1_sim_voltage.xacro > $URDF_SIM_VOLTAGE_PATH
-    xacro ghost_sim/urdf/ghost1_sim_pid.xacro > $URDF_SIM_PID_PATH
-    echo "URDF written to" $URDF_SIM_VOLTAGE_PATH
-    echo "URDF written to" $URDF_SIM_PID_PATH
+    if [ ! -d $GHOST_SIM_SHARE_DIR ];
+    then
+        touch $URDF_SIM_VOLTAGE_PATH
+        touch $URDF_SIM_PID_PATH
+    else
+        echo
+        echo "Generating URDF"
+        xacro ghost_sim/urdf/ghost1_sim_voltage.xacro > $URDF_SIM_VOLTAGE_PATH
+        xacro ghost_sim/urdf/ghost1_sim_pid.xacro > $URDF_SIM_PID_PATH
+        echo "URDF written to" $URDF_SIM_VOLTAGE_PATH
+        echo "URDF written to" $URDF_SIM_PID_PATH
+    fi
 fi
 
 source ~/.bashrc
