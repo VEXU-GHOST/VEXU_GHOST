e04b837eff90cdd2561fc6d59d9aa648a838c16f
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-10-31 22:59:28 -0500

    Adds yaml-cpp build to install deps script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,3 +2,12 @@
 sudo apt-get install -y libgoogle-glog-dev python3-colcon-common-extensions ros-foxy-xacro ros-foxy-gazebo-ros ros-foxy-gazebo-ros-pkgs
 sudo apt-get install -y ros-foxy-joint-state-publisher ros-foxy-joint-state-publisher-gui ros-foxy-rqt-tf-tree ros-foxy-joy-linux
 sudo apt-get install -y python3-pip
+
+cd ~/
+git clone git@github.com:jbeder/yaml-cpp.git
+cd yaml-cpp
+mkdir build
+cd build
+cmake -DYAML_BUILD_SHARED_LIBS=ON ..
+make -j
+sudo make install
