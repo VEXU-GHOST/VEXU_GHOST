e30b915cef715e944520e8a90a85a669cdf7ac8f
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-10-31 23:07:18 -0500

    adds LIDAR and RealSense ros pkgs to install deps

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get install -y libgoogle-glog-dev python3-colcon-common-extensions ros-foxy-xacro ros-foxy-gazebo-ros ros-foxy-gazebo-ros-pkgs
 sudo apt-get install -y ros-foxy-joint-state-publisher ros-foxy-joint-state-publisher-gui ros-foxy-rqt-tf-tree ros-foxy-joy-linux
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev
+sudo apt-get install -y ros-foxy-rplidar-ros  ros-foxy-realsense2*
 
 cd ~/
 git clone git@github.com:jbeder/yaml-cpp.git
@@ -11,3 +12,6 @@ cd build
 cmake -DYAML_BUILD_SHARED_LIBS=ON ..
 make -j
 sudo make install
+
+cd ~/VEXU_GHOST
+git submodule update --init
