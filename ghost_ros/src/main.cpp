a541603ec38776d68fccb2ef4d6dc7357eb976df
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-12-25 16:19:34 -0600

    begins refactor for ghost main modules

--- ghost_ros/src/main.cpp
+++ ghost_ros/src/main.cpp
@@ -18,9 +18,8 @@
 #include <yaml-cpp/yaml.h>
 
 #include "globals/globals.hpp"
-
-#include "ghost_modules/particle_filter_main.hpp"
-#include "ghost_modules/serial_main.hpp"
+#include "particle_filter/particle_filter_node.hpp"
+#include "ghost_serial/ghost_serial_node.hpp"
 
 using namespace std::literals::chrono_literals;
 
@@ -33,7 +32,9 @@ namespace globals{
 
 void SignalHandler(int) {
     if(rclcpp::ok()){
+        std::cout << "RCLCPP Shutdown" << std::endl;
         rclcpp::shutdown();
+
     }
 
     if (!globals::run) {
@@ -45,6 +46,27 @@ void SignalHandler(int) {
     globals::run = false;
 }
 
+// Particle Filter Main Thread
+void particle_filter_main(std::string config_file){
+    rclcpp::spin(std::make_shared<particle_filter::ParticleFilterNode>(config_file));
+}
+
+void serial_interface_main(std::string config_file)
+{
+    auto serial_node = std::make_shared<ghost_serial::GhostSerialNode>(config_file);
+    std::cout << "Init" << std::endl;
+    // // Open serial port
+    // bool serial_open = false;
+    // while(globals::run && !serial_open){
+    //     serial_open = serial_node->trySerialOpen();
+    // }
+
+    // serial_node->startReaderThread();
+
+    // rclcpp::spin(serial_node);
+    std::cout << "End" << std::endl;
+}
+
 int main(int argc, char* argv[]){
     globals::program_start_time = std::chrono::system_clock::now();
     
@@ -53,19 +75,21 @@ int main(int argc, char* argv[]){
     
     globals::repo_base_dir = std::string(getenv("HOME")) + "/VEXU_GHOST/";
 
-    // Initialize modules
-    std::thread particle_filter_thread(
-        ghost_modules::ghost_particle_filter_main,
-        globals::repo_base_dir + "ghost_ros/config/particle_filter.yaml"
-        );
-
-    std::thread v5_serial_interfaces(
-        ghost_modules::ghost_serial_main,
+    // // Initialize modules
+    // std::thread particle_filter_thread(
+    //     particle_filter_main,
+    //     globals::repo_base_dir + "ghost_ros/config/particle_filter.yaml"
+    //     );
+    
+    std::thread serial_interface_thread(
+        serial_interface_main,
         globals::repo_base_dir + "ghost_ros/config/ghost_serial.yaml"
     );
 
     // particle_filter_thread.join();
-    v5_serial_interfaces.join();
+    std::cout << "Gonna Join" << std::endl;
+    serial_interface_thread.join();
+    std::cout << "Joined" << std::endl;
 
     return 0;
 }
