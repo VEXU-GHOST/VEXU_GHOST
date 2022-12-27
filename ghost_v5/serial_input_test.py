1599a58266e5401b9105cfb296d2a685ba8068c3
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-12-27 03:04:51 -0600

    adds signal handler to serial python test script

--- ghost_v5/serial_input_test.py
+++ ghost_v5/serial_input_test.py
@@ -1,34 +1,46 @@
 import serial
 import time
 from cobs import cobs
+import signal
 
-ser = serial.Serial('/dev/ttyACM1')  # open serial port
-ser.flushInput()
-while(True):
-    buffer = ser.read_until(b'\x00')
-    print(buffer)
-    decoded_packet = cobs.decode(buffer[:-1])
-    print(decoded_packet)
+global run_
+run_ = True
 
-    # btns = int.from_bytes(decoded_packet[-4:-2], "little")
-    # print(decoded_packet[-4:])
-    # print(len(decoded_packet[-4:-2]))
-    # print(bin(btns))
-    # print(btns)
-    # print(btns & 0x8000, end = "")
-    # print(btns & 0x4000, end = "")
-    # print(btns & 0x2000, end = "")
-    # print(btns & 0x1000, end = "")
-    # print(btns & 0x0800, end = "")
-    # print(btns & 0x0400, end = "")
-    # print(btns & 0x0200, end = "")
-    # print(btns & 0x0100, end = "")
-    # print(btns & 0x0080, end = "")
-    # print(btns & 0x0040, end = "")
-    # print(btns & 0x0020, end = "")
-    # print(btns & 0x0010, end = "")
-    print()
+def handler(signum, frame):
+    print("Signal Handler")
+    global run_
+    run_ = False
 
-    print()
-    print()
-ser.close()             # close portc
+def main():
+    global run_
+    signal.signal(signal.SIGINT, handler)
+
+    ser = serial.Serial('/dev/ttyACM1', timeout=0.5)  # open serial port
+    ser.flushInput()
+    while(run_):
+        buffer = ser.read_until(b'\x00')
+        if(len(buffer) != 0):
+            print(buffer)
+            decoded_packet = cobs.decode(buffer[:-1])
+            print(decoded_packet)
+
+            # btns = int.from_bytes(decoded_packet[-4:-2], "little")
+            # print(decoded_packet[-4:])
+            # print(len(decoded_packet[-4:-2]))
+            # print(bin(btns))
+            # print(btns)
+            # print(btns & 0x8000, end = "")
+            # print(btns & 0x4000, end = "")
+            # print(btns & 0x2000, end = "")
+            # print(btns & 0x1000, end = "")
+            # print(btns & 0x0800, end = "")
+            # print(btns & 0x0400, end = "")
+            # print(btns & 0x0200, end = "")
+            # print(btns & 0x0100, end = "")
+            # print(btns & 0x0080, end = "")
+            # print(btns & 0x0040, end = "")
+            # print(btns & 0x0020, end = "")
+            # print(btns & 0x0010, end = "")
+    ser.close()             # close port
+
+main()
