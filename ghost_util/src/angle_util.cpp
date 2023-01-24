9f58b3e6f9167cb515b6fc90a8a56e836a242be4
Maxx Wilson <jessemaxxwilson@utexas.edu>
2023-01-24 17:28:23 -0600

    migrates examples to new ghost_util pkg adds angle utilities w unit tests

---
+++ ghost_util/src/angle_util.cpp
@@ -0,0 +1,44 @@
+#include "ghost_util/angle_util.hpp"
+
+namespace ghost_util
+{
+    float WrapAngle360(float angle){
+        angle = fmod(angle, 360.0);
+
+        if(angle < 0.0){
+            angle += 360.0;
+        }
+
+        return angle;
+    }
+
+    float WrapAngle180(float angle){
+        angle = WrapAngle360(angle);
+
+        if(angle > 180.0){
+            angle -= 360.0;
+        }
+
+        return angle;
+    }
+
+    float FlipAngle180(float angle){
+        return WrapAngle180(angle + 180.0);
+    }
+
+    float SmallestAngleDist360(float a2, float a1){
+        a1 = WrapAngle360(a1);
+        a2 = WrapAngle360(a2);
+
+        float diff = a1 - a2;
+        if(diff > 180.0){
+            diff = -(360.0 - diff);
+        }
+        else if(diff < -180.0){
+            diff = 360.0 + diff;
+        }
+        return diff;
+    }
+}
+
+
---
+++ ghost_util/test/test_angle_util.cpp
@@ -0,0 +1,74 @@
+#include "gtest/gtest.h"
+#include "ghost_util/angle_util.hpp"
+
+using namespace ghost_util;
+
+class TestAngleUtil: public ::testing::Test {
+    protected:
+
+    void SetUp() override {
+    }
+};
+
+TEST_F(TestAngleUtil, testWrapAngle360){
+  EXPECT_NEAR(5.0,    WrapAngle360(5.0), 0.01);
+  EXPECT_NEAR(5.0,    WrapAngle360(360.0 + 5.0), 0.01);
+  EXPECT_NEAR(5.0,    WrapAngle360(360.0 + 360.0 + 5.0), 0.01);
+  EXPECT_NEAR(5.0,    WrapAngle360(-360.0 + 5.0), 0.01);
+  EXPECT_NEAR(5.0,    WrapAngle360(-360.0 - 360.0 + 5.0), 0.01);
+  EXPECT_NEAR(181.0,  WrapAngle360(181.0), 0.01);
+  EXPECT_NEAR(179.0,  WrapAngle360(-181.0), 0.01);
+}
+
+TEST_F(TestAngleUtil, testWrapAngle180){
+  EXPECT_NEAR(1.0,    WrapAngle180(1.0), 0.01);
+
+  EXPECT_NEAR(179.0,  WrapAngle180(179.0), 0.01);
+  EXPECT_NEAR(-179.0, WrapAngle180(-179.0), 0.01);
+  
+  EXPECT_NEAR(-179.0, WrapAngle180(181.0), 0.01);
+  EXPECT_NEAR(179.0,  WrapAngle180(-181.0), 0.01);
+
+  EXPECT_NEAR(-175.0, WrapAngle180(185.0), 0.01);
+  EXPECT_NEAR(-175.0, WrapAngle180(185.0 + 360.0), 0.01);
+
+  EXPECT_NEAR(175.0,  WrapAngle180(-185.0), 0.01);
+  EXPECT_NEAR(175.0,  WrapAngle180(-185.0 - 360.0), 0.01);
+}
+
+
+TEST_F(TestAngleUtil, testFlipAngle180){
+  EXPECT_NEAR(179.0,  FlipAngle180(-1), 0.01);
+  EXPECT_NEAR(-1.0,   FlipAngle180(179.0), 0.01);
+  
+  EXPECT_NEAR(-179.0, FlipAngle180(1), 0.01);
+  EXPECT_NEAR(1.0,    FlipAngle180(-179.0), 0.01);
+  
+  EXPECT_NEAR(-135.0, FlipAngle180(45), 0.01);
+  EXPECT_NEAR(45.0,   FlipAngle180(-135.0), 0.01);
+ 
+  EXPECT_NEAR(135.0,  FlipAngle180(-45), 0.01);
+  EXPECT_NEAR(-45.0,  FlipAngle180(135.0), 0.01);
+}
+
+TEST_F(TestAngleUtil, testSmallestAngleDist360){
+  EXPECT_NEAR(-2.0,   SmallestAngleDist360(3.0, 1.0), 0.01);
+  EXPECT_NEAR(-2.0,   SmallestAngleDist360(1.0, -1.0), 0.01);
+
+  EXPECT_NEAR(90.0,   SmallestAngleDist360(1, 91.0), 0.01);
+  EXPECT_NEAR(-90.0,  SmallestAngleDist360(91.0, 1.0), 0.01);
+
+  EXPECT_NEAR(178.0,  SmallestAngleDist360(91.0, -91.0), 0.01);
+  EXPECT_NEAR(-178.0, SmallestAngleDist360(-91.0, 91.0), 0.01);
+
+  EXPECT_NEAR(2.0,    SmallestAngleDist360(179.0, -179.0), 0.01);
+  EXPECT_NEAR(-2.0,   SmallestAngleDist360(-179.0, 179.0), 0.01);
+  
+  EXPECT_NEAR(2.0,    SmallestAngleDist360(359.0, 1.0), 0.01);
+  EXPECT_NEAR(-2.0,   SmallestAngleDist360(1.0, 359.0), 0.01);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
