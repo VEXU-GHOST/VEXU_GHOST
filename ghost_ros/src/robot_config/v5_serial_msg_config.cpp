fd5829bb9b45725e139f82d952b001450c478856
Maxx Wilson <jessemaxxwilson@utexas.edu>
2023-03-01 20:41:54 -0600

    moves serial msg length calc to msg_config.cpp

--- ghost_ros/src/robot_config/v5_serial_msg_config.cpp
+++ ghost_ros/src/robot_config/v5_serial_msg_config.cpp
@@ -3,23 +3,24 @@
 namespace ghost_v5_config{
     // Defines order to pack Jetson -> V5 serial msg
     const std::vector<std::pair<v5_motor_id_enum, bool>> actuator_command_config = {
-        std::pair<v5_motor_id_enum, bool>(DRIVE_LEFT_FRONT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_LEFT_BACK_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_RIGHT_FRONT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_RIGHT_BACK_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_LEFT_REAR_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_LEFT_FRONT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_RIGHT_REAR_MOTOR, false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_LEFT_FRONT_MOTOR,       false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_LEFT_BACK_MOTOR,        false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_RIGHT_FRONT_MOTOR,      false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_RIGHT_BACK_MOTOR,       false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_LEFT_REAR_MOTOR,   false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_LEFT_FRONT_MOTOR,  false),
+        std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_RIGHT_REAR_MOTOR,  false),
         std::pair<v5_motor_id_enum, bool>(DRIVE_BACK_RIGHT_FRONT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(TURRET_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(INTAKE_LEFT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(INTAKE_RIGHT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(INDEXER_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(SHOOTER_LEFT_MOTOR, false),
-        std::pair<v5_motor_id_enum, bool>(SHOOTER_RIGHT_MOTOR, false),
+        std::pair<v5_motor_id_enum, bool>(TURRET_MOTOR,                 false),
+        std::pair<v5_motor_id_enum, bool>(INTAKE_LEFT_MOTOR,            false),
+        std::pair<v5_motor_id_enum, bool>(INTAKE_RIGHT_MOTOR,           false),
+        std::pair<v5_motor_id_enum, bool>(INDEXER_MOTOR,                true),
+        std::pair<v5_motor_id_enum, bool>(SHOOTER_LEFT_MOTOR,           false),
+        std::pair<v5_motor_id_enum, bool>(SHOOTER_RIGHT_MOTOR,          false),
     };
-
-    const int actuator_cmd_extra_byte_count = 4; // Msg Sequence ID
+    // Each motor has current, velocity, and voltage (position is added separately)
+    const int actuator_update_packet_byte_size = 3 * 4;
+    const int actuator_cmd_extra_byte_count = 4 + 4; // 4x Bytes for Msg Sequence ID + 4x Bytes for Motor Active Vector
 
     // Defines order to pack V5 -> Jetson serial msg
     const std::vector<v5_motor_id_enum> sensor_update_motor_config = {
@@ -43,7 +44,11 @@ namespace ghost_v5_config{
         // TURRET_ENCODER
     };
 
-    // 4x int32 Joystick Channels, 2x bytes of btns/competition modes, 4x Bytes of port status, Msg Sequence ID
+    // Each motor reports angle_degrees, velocity_rpm, voltage_mv, current_ma, power_w, temp_c  
+    const int motor_sensor_packet_byte_size = 6 * 4; 
+    const int encoder_sensor_packet_byte_size = 2 * 4; 
+
+    // 4x int32 Joystick Channels, 2x bytes of btns/competition modes, 4x Bytes of port status, 4x Bytes of Msg Sequence ID
     const int sensor_update_extra_byte_count = 4*4 + 2 + 4 + 4;
 
     // Maps device enum to device_name
@@ -67,4 +72,22 @@ namespace ghost_v5_config{
         {STEERING_BACK_ENCODER,         "STEERING_BACK_ENCODER"},
         // {TURRET_ENCODER,                "TURRET_ENCODER"},
     };
+
+    int get_actuator_command_msg_len(){
+        int msg_len_ = actuator_update_packet_byte_size * actuator_command_config.size();
+        for (auto &pair : actuator_command_config)
+        {
+            msg_len_ += (pair.second) ? 4 : 0; // Add four bytes for each motor using position control
+        }
+        msg_len_ += actuator_cmd_extra_byte_count;
+        return msg_len_;
+    }
+
+    int get_sensor_update_msg_len(){
+        int msg_len_ = 
+			sensor_update_motor_config.size() * motor_sensor_packet_byte_size +
+			sensor_update_sensor_config.size() * encoder_sensor_packet_byte_size;
+		msg_len_ += ghost_v5_config::sensor_update_extra_byte_count;
+        return msg_len_;
+    }
 }
