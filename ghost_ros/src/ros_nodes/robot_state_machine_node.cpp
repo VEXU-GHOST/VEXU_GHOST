00b09506ef2c74f782273ed0a3916437a426042a
xwilson03 <xanderwilson2014@gmail.com>
2023-04-26 21:00:12 -0500

    push to laptop to fix wheel misdirection

--- ghost_ros/src/ros_nodes/robot_state_machine_node.cpp
+++ ghost_ros/src/ros_nodes/robot_state_machine_node.cpp
@@ -262,8 +262,6 @@ namespace ghost_ros
                 actuator_cmd_msg_.motor_commands[ghost_v5_config::INDEXER_MOTOR].desired_voltage = 1.0;
                 actuator_cmd_msg_.motor_commands[ghost_v5_config::INDEXER_MOTOR].current_limit = 2500;
                 actuator_cmd_msg_.motor_commands[ghost_v5_config::INDEXER_MOTOR].active = true;
-
-
                 }
 
             break;
