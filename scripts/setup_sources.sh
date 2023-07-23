1026f577c810681cfa8c5f5e85c06dca5065c873
Maxx Wilson <jessemaxxwilson@utexas.edu>
2023-07-23 01:33:14 -0500

    fixes setup sources still using foxy and ghost_deps adds rosdep init and update to update_deps

--- scripts/setup_sources.sh
+++ scripts/setup_sources.sh
@@ -1,5 +1,4 @@
 (
-        echo "source /opt/ros/foxy/setup.bash"
+        echo "source /opt/ros/humble/setup.bash"
 	echo "source ~/VEXU_GHOST/install/setup.bash"
-	echo "source ~/ghost_deps/rplidar_ros/install/setup.bash"
 ) >> ~/.bashrc
--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd ~/VEXU_GHOST
+
+echo
+echo "--------------- ROSDEP Init ---------------"
+sudo rosdep init
+
+echo
+echo "--------------- ROSDEP Update ---------------"
+rosdep update
+
+echo
+echo "--------------- ROSDEP Install ---------------"
 rosdep install --from-paths . --ignore-src -r -y
