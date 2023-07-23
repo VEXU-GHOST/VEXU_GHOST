cd6820645e59d9e1584277f7c4c5f2d569807bcb
MELISSA CRUZ <melissajecruz@utexas.edu>
2023-07-23 18:25:03 -0500

    comment out documentation

--- ghost_estimation/src/filters/ext_kalman_filter.cpp
+++ ghost_estimation/src/filters/ext_kalman_filter.cpp
@@ -16,37 +16,37 @@ namespace ghost_estimation
         private:
         // Predict Step
         // Estimated state. 0 is current state, 1 is predicted state
-        xk_0
-        xk_1
+        // xk_0
+        // xk_1
          
-        // Estimated covariance
-        Pk
+        // // Estimated covariance
+        // Pk
 
-        // Prediction Matrix
-        Fk
+        // // Prediction Matrix
+        // Fk
 
-        // Control Matrix
-        Bk
+        // // Control Matrix
+        // Bk
         
-        // Control Vector
-        uk
+        // // Control Vector
+        // uk
 
-        // Noise
-        Qk
+        // // Noise
+        // Qk
 
-        // Update Step
-        // Covariance of sensors (sensor noise)
-        Rk 
+        // // Update Step
+        // // Covariance of sensors (sensor noise)
+        // Rk 
 
-        // Define Covariance Matrices
-        E0 = Pk * Hk^T
-        E1 = E0 - K * E0
+        // // Define Covariance Matrices
+        // E0 = Pk * Hk^T
+        // E1 = E0 - K * E0
 
-        // Define Kalman Gain K
-        K = E0 * (Hk^T * E0 + E1)^-1
+        // // Define Kalman Gain K
+        // K = E0 * (Hk^T * E0 + E1)^-1
 
-        // Calculate next state and next state covariance
-        xk_1 = xk_0 + K * (zk - Hk * xk_0)
-        Pk_1 = Pk_0 - K * Hk * Pk_0
+        // // Calculate next state and next state covariance
+        // xk_1 = xk_0 + K * (zk - Hk * xk_0)
+        // Pk_1 = Pk_0 - K * Hk * Pk_0
     }
 }
