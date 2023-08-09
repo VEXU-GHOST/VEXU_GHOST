55b0f3437e172e9b78aadf377d383829c16abd2e
JakeWendling <jakewendling57@gmail.com>
2023-08-09 18:10:20 -0500

    changes disc detector to obj detector

--- ghost_estimation/src/disc_detector/marker_generator.py
+++ ghost_estimation/src/disc_detector/marker_generator.py
@@ -5,44 +5,38 @@ import rclpy # Python library for ROS 2
 #import math
 from rclpy.node import Node # Handles the creation of nodes
 from geometry_msgs.msg import PoseWithCovariance #, Pose
-from ghost_msgs.msg import CVDiscList
+from ghost_msgs.msg import CVObjList
 from visualization_msgs.msg import Marker, MarkerArray
 
-class DiscMarkerGeneratorNode(Node):
+class MarkerGeneratorNode(Node):
     def __init__(self):
-        super().__init__('disc_detector_node')
-        self.disc_subscription = self.create_subscription(
+        super().__init__('marker_generator_node')
+        self.obj_subscription = self.create_subscription(
             #sensor_msgs/msg/Image,
-            CVDiscList,
-            '/cv_discs',
-            self.disc_listener_callback,
+            CVObjList,
+            '/cv_objs',
+            self.obj_listener_callback,
             10)
-        self.marker_publisher = self.create_publisher(MarkerArray, 'cv_disc_markers', 10)
+        self.marker_publisher = self.create_publisher(MarkerArray, 'cv_obj_markers', 10)
         self.log_string = ''
         self.header = None
         
-    def disc_listener_callback(self, disc_list):
-        self.get_logger().info('CVDiscList Callback')
-        discs = disc_list.discs
+    def obj_listener_callback(self, obj_list):
+        self.get_logger().info('CVObjList Callback')
+        objs = obj_list.objs
         marker_array = MarkerArray()
-        for disc in discs:
+        for obj in objs:
             marker = Marker()
-            marker.header = disc_list.header
+            marker.header = obj_list.header
             marker.header.frame_id = 'camera_base_link'
-            marker.ns = "cv_disc"
+            marker.ns = "cv_obj"
             marker.id = len(marker_array.markers)
             marker.type = Marker.CYLINDER
             marker.action = Marker.ADD
             marker.lifetime = rclpy.duration.Duration(seconds = 0.1).to_msg()
-            marker.pose.position.x = disc.pose.position.x
-            marker.pose.position.y = disc.pose.position.y
-            marker.pose.position.z = disc.pose.position.z 
-            marker.pose.orientation.x = disc.pose.orientation.x#0.0#0.707
-            marker.pose.orientation.y = disc.pose.orientation.y#0.0
-            marker.pose.orientation.z = disc.pose.orientation.z#0.0
-            marker.pose.orientation.w = disc.pose.orientation.w#1.0#0.707
-            marker.scale.x = disc.covariance[0]**.5
-            marker.scale.y = disc.covariance[1+6*1]**.5
+            marker.pose = obj.pose
+            marker.scale.x = obj.covariance[0]**.5
+            marker.scale.y = obj.covariance[1+6*1]**.5
             marker.scale.z = 0.01
             marker.color.a = 0.5 # Don't forget to set the alpha!
             marker.color.r = 0.0
@@ -55,11 +49,11 @@ class DiscMarkerGeneratorNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     
-    disc_marker_node = DiscMarkerGeneratorNode()
+    obj_marker_node = ObjMarkerGeneratorNode()
     
-    rclpy.spin(disc_marker_node)
+    rclpy.spin(obj_marker_node)
 
-    disc_marker_node.destroy_node()
+    obj_marker_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
--- ghost_estimation/src/disc_detector/obj_detector.py
+++ ghost_estimation/src/disc_detector/obj_detector.py
@@ -11,13 +11,13 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 from geometry_msgs.msg import PoseWithCovariance
-from ghost_msgs.msg import CVDisc, CVDiscList
+from ghost_msgs.msg import CVObjList
 from scipy.spatial.transform import Rotation as R
 from tf2_ros.transform_broadcaster import TransformBroadcaster
 
 cov_ratio = 1
 
-class Disc:
+class Object:
     def __init__(this, x, y, radius):
         this.x = x
         this.y = y
@@ -29,7 +29,7 @@ class Disc:
 
     
 
-class DiscDetector:
+class ObjectDetector:
     def __init__(this, hsize, vsize, hfov, vfov, lower_color, upper_color):
         this.hsize = hsize
         this.vsize = vsize
@@ -38,9 +38,9 @@ class DiscDetector:
         this.lower_color = lower_color
         this.upper_color = upper_color
 
-    def filterDisc(this, image):
+    def filterObject(this, image):
         '''
-        Filters the image using cv2 for disc colors (yellow)
+        Filters the image using cv2 for object colors (yellow)
 
         Parameters: 
             image - (numpy array) image from camera
@@ -55,33 +55,33 @@ class DiscDetector:
         image = cv2.erode(image, None, iterations=2)
         return image
 
-    def findDiscs(this, image):
+    def findObjects(this, image):
         '''
-        Finds discs in the image using cv2
+        Finds objects in the image using cv2
 
         Parameters: 
             image - (numpy array) filtered image from camera
             
         Returns: 
-            discs - (list(Discs)) list of discs found
+            objects - (list(Objects)) list of objects found
         '''
         contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)
         radius = 0
 
-        discs = []
+        objects = []
         for contour in contours:
             ((x, y), radius) = cv2.minEnclosingCircle(contour)
             if radius > 10:
-                discs.append(Disc(x, y, radius))
-        return discs
+                objects.append(Object(x, y, radius))
+        return objects
 
-    def addDiscToDisplay(this, image, disc: Disc, text: bool):
+    def addObjectToDisplay(this, image, object: Object, text: bool):
         '''
-        Adds disc info to the camera image
+        Adds object info to the camera image
 
         Parameters: 
-            disc - (Disc) disc to add info for
+            object - (Object) object to add info for
             image - (numpy array) image to add info to
             text - (boolean) whether to add text or only boxes
             
@@ -89,88 +89,88 @@ class DiscDetector:
             image - (numpy array) covariance of the angle
         '''
         meter_to_inch = 39.3701
-        if disc.radius > 10:
-            startpt = (int(disc.x) - int(disc.radius), int(disc.y) - int(disc.radius))
-            endpt = (int(disc.x) + int(disc.radius), int(disc.y) + int(disc.radius))
+        if object.radius > 10:
+            startpt = (int(object.x) - int(object.radius), int(object.y) - int(object.radius))
+            endpt = (int(object.x) + int(object.radius), int(object.y) + int(object.radius))
             image = cv2.rectangle(image, startpt, endpt, (255, 255, 0), 1)
             if text:
-                distance = round(disc.distance,3)
-                angle = round(disc.angle,3)
+                distance = round(object.distance,3)
+                angle = round(object.angle,3)
                 text_dist = f'distance: {distance}\"'
                 text_angle = f'angle: {angle}'
                 textList = [text_angle,text_dist]
                 for i in range(len(textList)):
-                    text_position = (int(disc.x) + int(disc.radius), int(disc.y) + int(disc.radius) + i*15)
+                    text_position = (int(object.x) + int(object.radius), int(object.y) + int(object.radius) + i*15)
                     image = cv2.putText(image, textList[i], text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1, cv2.LINE_AA)
         return image
 
-    def calcDistCovariance(self, disc, depth_image, unit_conversion) -> float:
+    def calcDistCovariance(self, object, depth_image, unit_conversion) -> float:
         '''
         Calculates distance covariance by finding the difference 
-        in distance at the top/bottom edges of the disc 
+        in distance at the top/bottom edges of the object 
 
         Parameters: 
-            disc - (Disc) disc to calculate angle covariance for
+            object - (Object) object to calculate angle covariance for
             depth_image - (np.array) depth image from camera
             unit_conversion - (float) ratio to convert mm to m 
             
         Returns: 
             angle covariance - (float) covariance of the angle
         '''
-        depth_max_y = depth_image[int(min(disc.y + disc.radius/2, 479))][int(disc.x)] * unit_conversion
-        depth_min_y = depth_image[int(max(disc.y - disc.radius/2, 0))][int(disc.x)] * unit_conversion
+        depth_max_y = depth_image[int(min(object.y + object.radius/2, 479))][int(object.x)] * unit_conversion
+        depth_min_y = depth_image[int(max(object.y - object.radius/2, 0))][int(object.x)] * unit_conversion
         return float(abs((depth_max_y - depth_min_y) * cov_ratio))
 
-    def calcAngle(this, disc, hsize=640, hfov=69) -> float:
+    def calcAngle(this, object, hsize=640, hfov=69) -> float:
         '''
-        Calculates the angle of a disc from the forward (x) axis
+        Calculates the angle of a object from the forward (x) axis
 
         Parameters: 
-            disc - (Disc) disc to calculate angle for
+            object - (Object) object to calculate angle for
             hsize - (int) horizontal size of image in pixels
             hfov - (int) horizontal size of camera view in degrees
             
         Returns: 
             angle - (float) angle from the forward axis
         '''
-        angle = -1*((disc.x - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
+        angle = -1*((object.x - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
         return angle
 
-    def calcAngleCovariance(this, disc, hsize=640, hfov=69) -> float:
+    def calcAngleCovariance(this, object, hsize=640, hfov=69) -> float:
         '''
         Calculates angle covariance by finding the difference 
-        in angle at the left/right edges of the disc 
+        in angle at the left/right edges of the object 
 
         Parameters: 
-            disc - (Disc) disc to calculate angle covariance for
+            object - (Object) object to calculate angle covariance for
             hsize - (int) horizontal size of image in pixels
             hfov - (int) horizontal size of camera view in degrees
             
         Returns: 
             angle covariance - (float) covariance of the angle
         '''
-        angle_min = ((disc.x + disc.radius/2 - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
-        angle_max = ((disc.x - disc.radius/2 - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
+        angle_min = ((object.x + object.radius/2 - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
+        angle_max = ((object.x - object.radius/2 - hsize/2)/(hsize/2))*(hfov/2)*math.pi/180
         return abs((angle_max - angle_min) * cov_ratio)
 
-    def detectDiscs(this, color_image: np.array, depth_image: np.array):
+    def detectObjects(this, color_image: np.array, depth_image: np.array):
         '''
-        Detects discs in the given image
+        Detects objects in the given image
 
         Parameters: 
             color_image - (numpy array) image from camera
             depth_image - (numpy array) depth image from camera
 
         Returns: 
-            color_image - (numpy array) image with added text/boxes around discs
-            discs -       (list(discs) list of discs detected
+            color_image - (numpy array) image with added text/boxes around objects
+            objects -       (list(objects) list of objects detected
         '''
         try:
             #starttime = time.perf_counter()
             unit_conversion = 1/1000
-            disc_image = this.filterDisc(color_image)
-            discs = this.findDiscs(disc_image)
-            for d in discs:
+            object_image = this.filterObject(color_image)
+            objects = this.findObjects(object_image)
+            for d in objects:
                 if not d:
                     continue
                 try:
@@ -181,21 +181,21 @@ class DiscDetector:
                     d.distance_covariance = this.calcDistCovariance(d, depth_image, unit_conversion)
                     d.angle = this.calcAngle(d)
                     d.angle_covariance = this.calcAngleCovariance(d)
-                color_image = this.addDiscToDisplay(color_image, d, True)
+                color_image = this.addObjectToDisplay(color_image, d, True)
             #fps = 1/(time.perf_counter() - starttime)
             #color_image = cv2.putText(color_image, f"fps: {fps:.2f}", (0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1, cv2.LINE_AA)
-            return color_image, discs
+            return color_image, objects
         finally:
             a=0
-        #return disc_image
-        #disc_detector.stopDisplay
+        #return object_image
+        #object_detector.stopDisplay
 
 def initCamera():
     '''
-    Initializes disc detector object
+    Initializes object detector object
 
     Returns: 
-        disc_detector - DiscDetector object
+        object_detector - ObjectDetector object
     '''
     hsize = 640
     vsize = 480
@@ -203,18 +203,18 @@ def initCamera():
     vfov = 42
     lower_color = (25,70,100)
     upper_color = (80,255,255)
-    disc_detector = DiscDetector(hsize, vsize, hfov, vfov, lower_color, upper_color)
-    return disc_detector
+    object_detector = ObjectDetector(hsize, vsize, hfov, vfov, lower_color, upper_color)
+    return object_detector
 
-class DiscDetectorNode(Node):
+class ObjectDetectorNode(Node):
     def __init__(self):
         '''
         Constructor
         Creates image and depth subscribers
-        Creates image and disc publishers
+        Creates image and object publishers
         Initializes all variables saved in the node object and timer
         '''
-        super().__init__('disc_detector_node')
+        super().__init__('object_detector_node')
         self.image_subscription = self.create_subscription(
             Image,                                         
             '/camera/color/image_raw',                     
@@ -230,16 +230,16 @@ class DiscDetectorNode(Node):
             Image, 
             'cv_frames', 
             10)
-        self.disc_detector = initCamera()
+        self.object_detector = initCamera()
         self.br = CvBridge()
         self.cv_image = None
         self.depth_image = None
         
-        self.disc_publisher_ = self.create_publisher(
-            CVDiscList, 
-            'cv_discs', 
+        self.object_publisher_ = self.create_publisher(
+            CVObjList, 
+            'cv_objects', 
             10)
-        self.discs = []
+        self.objects = []
         
         self.log_string = ''
         self.header = None
@@ -249,11 +249,11 @@ class DiscDetectorNode(Node):
     def timer_callback(self):
         '''
         Callback function when the timer is triggered
-        Publishes the image and discs saved in the node object
+        Publishes the image and objects saved in the node object
         '''
         self.log_string = ''
         
-        #publish image with boxes around discs
+        #publish image with boxes around objects
         try:
             if not self.cv_image:
                 self.log_string += 'Video frame not found, '
@@ -261,39 +261,39 @@ class DiscDetectorNode(Node):
             self.image_publisher_.publish(self.br.cv2_to_imgmsg(self.cv_image,encoding="rgb8"))
             self.log_string += 'Publishing video frame, '
         
-        #publish DiscList
-        if not self.discs:
-            self.log_string += 'Discs not found, '
+        #publish ObjectList
+        if not self.objects:
+            self.log_string += 'Objects not found, '
         else: 
             #ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:="640X480X15" enable_depth:=true depth_module.profile:="640X480X15"
-            cv_disc_list = CVDiscList()
-            cv_disc_list.header = self.header
-            for d in self.discs:
-                cv_disc = PoseWithCovariance()
-                cv_disc.pose.position.y = d.distance * math.sin(d.angle)
-                cv_disc.pose.position.x = d.distance * math.cos(d.angle)
-                cv_disc.pose.position.z = -0.38 #-15 in m # 0.0
+            cv_object_list = CVObjList()
+            cv_object_list.header = self.header
+            for d in self.objects:
+                cv_object = PoseWithCovariance()
+                cv_object.pose.position.y = d.distance * math.sin(d.angle)
+                cv_object.pose.position.x = d.distance * math.cos(d.angle)
+                cv_object.pose.position.z = -0.38 #-15 in m # 0.0
                 r = R.from_euler('xyz', [0, 0, d.angle])
-                cv_disc.pose.orientation.x = r.as_quat()[0]#0.0#0.707
-                cv_disc.pose.orientation.y = r.as_quat()[1]#0.0
-                cv_disc.pose.orientation.z = r.as_quat()[2]#0.0
-                cv_disc.pose.orientation.w = r.as_quat()[3]#1.0#0.707
+                cv_object.pose.orientation.x = r.as_quat()[0]#0.0#0.707
+                cv_object.pose.orientation.y = r.as_quat()[1]#0.0
+                cv_object.pose.orientation.z = r.as_quat()[2]#0.0
+                cv_object.pose.orientation.w = r.as_quat()[3]#1.0#0.707
                 covariance = np.zeros(36)
                 covariance[0] = d.distance_covariance**2
                 covariance[1+6*1] = (d.angle_covariance*d.distance)**2+(d.angle*d.distance_covariance)**2
-                cv_disc.covariance = covariance
-                cv_disc_list.discs.append(cv_disc)
-            self.log_string += f'{len(cv_disc_list.discs)} discs found, '
-            cv_disc_list.num_discs = len(cv_disc_list.discs)
-            self.disc_publisher_.publish(cv_disc_list)
+                cv_object.covariance = covariance
+                cv_object_list.objects.append(cv_object)
+            self.log_string += f'{len(cv_object_list.objects)} objects found, '
+            cv_object_list.num_objects = len(cv_object_list.objects)
+            self.object_publisher_.publish(cv_object_list)
                     
         self.get_logger().info(self.log_string)
 
     def image_listener_callback(self, data):
         '''
         Callback function when image from camera is recieved
-        Calls detectDiscs function using the image and the depth image
-        Saves the image with boxes around discs in the node
+        Calls detectObjects function using the image and the depth image
+        Saves the image with boxes around objects in the node
 
         Parameters: 
             data - image from camera (imgmsg)
@@ -301,7 +301,7 @@ class DiscDetectorNode(Node):
         self.get_logger().info('Camera Callback')
         self.header = data.header
         frame = self.br.imgmsg_to_cv2(data)
-        self.cv_image, self.discs = self.disc_detector.detectDiscs(frame, self.depth_image)  
+        self.cv_image, self.objects = self.object_detector.detectObjects(frame, self.depth_image)  
 
     def depth_listener_callback(self, data):
         '''
@@ -316,11 +316,11 @@ class DiscDetectorNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     
-    disc_detector_node = DiscDetectorNode()
+    object_detector_node = ObjectDetectorNode()
     
-    rclpy.spin(disc_detector_node)
+    rclpy.spin(object_detector_node)
 
-    disc_detector_node.destroy_node()
+    object_detector_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
