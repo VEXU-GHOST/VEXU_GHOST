b64fa46311bcb34860ddd72d86cbf5fa09a5bc4c
xwilson03 <xanderwilson2014@gmail.com>
2023-10-15 21:47:51 -0500

    reverts v5_robot_config to three wheel chassis

--- 01_Libraries/ghost_common/src/v5_robot_config.cpp
+++ 01_Libraries/ghost_common/src/v5_robot_config.cpp
@@ -31,23 +31,23 @@ const MotorConfigStruct intake_motor_config = {
 ////////// Motor Definitions //////////
 ///////////////////////////////////////
 const std::unordered_map<std::string, motor_access_helper> motor_config_map{
-	{"DRIVE_LEFT_FRONT_LEFT_MOTOR",  motor_access_helper(1, false, drive_motor_config)},
-	{"DRIVE_LEFT_FRONT_RIGHT_MOTOR",  motor_access_helper(2, false, drive_motor_config)},
-	{"DRIVE_LEFT_BACK_LEFT_MOTOR",   motor_access_helper(3, false, drive_motor_config)},
-	{"DRIVE_LEFT_BACK_RIGHT_MOTOR",   motor_access_helper(4, false, drive_motor_config)},
-	{"DRIVE_RIGHT_FRONT_LEFT_MOTOR",  motor_access_helper(5, false, drive_motor_config)},
-	{"DRIVE_RIGHT_FRONT_RIGHT_MOTOR",  motor_access_helper(6, false, drive_motor_config)},
-	{"DRIVE_RIGHT_BACK_LEFT_MOTOR",  motor_access_helper(7, false, drive_motor_config)},
-	{"DRIVE_RIGHT_BACK_RIGHT_MOTOR",  motor_access_helper(8, false, drive_motor_config)},
+	{"DRIVE_LEFT_FRONT_MOTOR",       motor_access_helper(3,  false, drive_motor_config)},
+	{"DRIVE_LEFT_BACK_MOTOR",        motor_access_helper(4,  false, drive_motor_config)},
+	{"DRIVE_BACK_LEFT_FRONT_MOTOR",  motor_access_helper(5,  false, drive_motor_config)},
+	{"DRIVE_BACK_LEFT_REAR_MOTOR",   motor_access_helper(6,  false, drive_motor_config)},
+	{"DRIVE_BACK_RIGHT_REAR_MOTOR",  motor_access_helper(7,  false, drive_motor_config)},
+	{"DRIVE_BACK_RIGHT_FRONT_MOTOR", motor_access_helper(8,  false, drive_motor_config)},
+	{"DRIVE_RIGHT_BACK_MOTOR",       motor_access_helper(9,  false, drive_motor_config)},
+	{"DRIVE_RIGHT_FRONT_MOTOR",      motor_access_helper(10, false, drive_motor_config)},
 };
 
 /////////////////////////////////////////
 ////////// Encoder Definitions //////////
 /////////////////////////////////////////
 const std::unordered_map<std::string, encoder_access_helper> encoder_config_map{
-	{"STEERING_LEFT_ENCODER",     encoder_access_helper(9, true)},
-	{"STEERING_RIGHT_ENCODER",    encoder_access_helper(10, true)},
-	{"STEERING_BACK_ENCODER",      encoder_access_helper(11, true)},
+	{"STEERING_LEFT_ENCODER",     encoder_access_helper(1, true)},
+	{"STEERING_RIGHT_ENCODER",    encoder_access_helper(2, true)},
+	{"STEERING_BACK_ENCODER",      encoder_access_helper(12, true)},
 };
 
 } // namespace ghost_v5_config
