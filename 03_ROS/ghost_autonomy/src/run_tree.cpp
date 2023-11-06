395e28cd021ac8165ebeba4b545c459e60c00f73
JakeWendling <jakewendling57@gmail.com>
2023-11-05 18:39:12 -0600

    adds launch file for run tree

--- 03_ROS/ghost_autonomy/src/run_tree.cpp
+++ 03_ROS/ghost_autonomy/src/run_tree.cpp
@@ -1,16 +1,71 @@
 #include "behaviortree_cpp/bt_factory.h"
+// #include <filesystem>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "test.cpp"
 
 // file that contains the custom nodes definitions
 // #include "dummy_nodes.h"
 // using namespace DummyNodes;
 
-int main()
-{  
-  BT::BehaviorTreeFactory factory;
-//   factory.registerNodeType<SaySomething>("SaySomething");
-//   factory.registerNodeType<ThinkWhatToSay>("ThinkWhatToSay");
+class RunTreeNode : public rclcpp::Node {
+public:
+		RunTreeNode() :
+				rclcpp::Node("run_tree_node"){
+        declare_parameter<std::string>("bt_path");
+				bt_path_ = get_parameter("bt_path").as_string();
 
-  auto tree = factory.createTreeFromFile("../my_tree.xml");
-  tree.tickWhileRunning();
-  return 0;
+				start_sub_ = create_subscription<std_msgs::msg::Bool>(
+						"run_tree",
+						10,
+						[this](const std_msgs::msg::Bool::SharedPtr msg){
+						if(msg->data){
+								run_tree();
+						}
+				});
+        run_tree();//remove
+		}
+
+private:
+		std::string bt_path_;
+
+		rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr start_sub_;
+
+		void run_tree(){
+        // todo: add ros node to blackboard
+        // todo: move this to another class an import
+				BT::BehaviorTreeFactory factory;
+				factory.registerNodeType<SaySomething>("SaySomething");
+				//   factory.registerNodeType<ThinkWhatToSay>("ThinkWhatToSay");
+				// std::cout << std::filesystem::current_path();
+				auto tree = factory.createTreeFromFile(bt_path_);
+				tree.tickWhileRunning();
+		}
+};
+
+// class RunTreePublisher : public rclcpp::Node {
+// public:
+// 		RunTreePublisher() :
+// 				Node("run_tree_publisher"){
+// 				// rclcpp::TimerBase::SharedPtr timer_;
+
+// 				// Initialize Publisher
+// 				output_pub_ = this->create_publisher<std_msgs::msg::Bool>("run_tree", 10);
+// 		}
+// 		void startTree(){
+// 				std_msgs::msg::Bool msg;
+// 				msg.data = true;
+// 				output_pub_->publish(msg);
+// 		}
+// private:
+// 		rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr output_pub_;
+// };
+
+int main(int argc, char *argv[]){
+		rclcpp::init(argc, argv);
+		auto node = std::make_shared<RunTreeNode>();
+		rclcpp::spin(node);
+
+		rclcpp::shutdown();
+		return 0;
 }
--- 03_ROS/ghost_autonomy/src/test.cpp
+++ 03_ROS/ghost_autonomy/src/test.cpp
@@ -1,16 +1,33 @@
 #include "behaviortree_cpp/behavior_tree.h"
 
-class ApproachObject : public BT::SyncActionNode
+// SyncActionNode (synchronous action) with an input port.
+class SaySomething : public BT::SyncActionNode
 {
 public:
-  ApproachObject(const std::string& name) :
-      BT::SyncActionNode(name, {})
-  {}
+  // If your Node has ports, you must use this constructor signature 
+  SaySomething(const std::string& name, const BT::NodeConfig& config)
+    : SyncActionNode(name, config)
+  { }
 
-  // You must override the virtual function tick()
+  // It is mandatory to define this STATIC method.
+  static BT::PortsList providedPorts()
+  {
+    // This action has a single input port called "message"
+    return { /*BT::InputPort<std::string>("message")*/ };
+  }
+
+  // Override the virtual function tick()
   BT::NodeStatus tick() override
   {
-    std::cout << "ApproachObject: " << this->name() << std::endl;
+    // Expected<std::string> msg = getInput<std::string>("message");
+    // // Check if expected is valid. If not, throw its error
+    // if (!msg)
+    // {
+    //   throw BT::RuntimeError("missing required input [message]: ", 
+    //                           msg.error() );
+    // }
+    // use the method value() to extract the valid message.
+    
     return BT::NodeStatus::SUCCESS;
   }
 };
