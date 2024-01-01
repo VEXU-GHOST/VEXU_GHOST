6b4b333f63fa1f74a2576343df17e5043a4d1335
xwilson03 <xanderwilson2014@gmail.com>
2024-01-01 05:25:00 -0600

    makes dependency script error checking more strict

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-exit_code=0
-
 # Verify repo path is set
 if [ -z "${VEXU_HOME}" ]
 then
@@ -15,24 +13,24 @@ echo
 echo "--------------- Non-ROS Dependencies ---------------"
 sudo apt-get install -y libgoogle-glog-dev python3-colcon-common-extensions gfortran-10
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev python3-rosdep2 apt-rdepends
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 pip install colcon-lint
 
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 
@@ -40,27 +38,27 @@ echo
 echo "--------------- ROSDEP Init ---------------"
 sudo rosdep init
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 echo
 echo "--------------- ROSDEP Update ---------------"
 rosdep update
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 echo
 echo "--------------- ROSDEP Install ---------------"
 rosdep install --from-paths . --ignore-src -r -y
 
-# Update exit code if step failed
+# Exit if step failed
 if [[ $? -ne 0 ]]; then
-    exit_code=-1
+    exit -1
 fi
 
 exit $exit_code
