b36153ae9cb9d4c90eeb3a995e1df2c23115cebc
xwilson03 <xanderwilson2014@gmail.com>
2024-01-01 05:10:44 -0600

    adds error checking to update_dependencies

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exit_code=0
+
 # Verify repo path is set
 if [ -z "${VEXU_HOME}" ]
 then
@@ -12,17 +14,53 @@ cd $VEXU_HOME
 echo
 echo "--------------- Non-ROS Dependencies ---------------"
 sudo apt-get install -y libgoogle-glog-dev python3-colcon-common-extensions gfortran-10
+
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
 sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev python3-rosdep2 apt-rdepends
+
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
 pip install colcon-lint
+
+
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
 
 echo
 echo "--------------- ROSDEP Init ---------------"
 sudo rosdep init
 
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
 echo
 echo "--------------- ROSDEP Update ---------------"
 rosdep update
 
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
 echo
 echo "--------------- ROSDEP Install ---------------"
 rosdep install --from-paths . --ignore-src -r -y
+
+# Update exit code if step failed
+if [[ $? -ne 0 ]]; then
+    exit_code=-1
+fi
+
+exit $exit_code
