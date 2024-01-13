a2ac95f70f32ce9b61e9306cfd470566b5d5bd95
Zaara <zbilal6135@gmail.com>
2024-01-12 21:20:50 -0600

    Adds four wheel swerve urdf

--- 11_Robots/ghost_over_under/launch/rviz.launch.py
+++ 11_Robots/ghost_over_under/launch/rviz.launch.py
@@ -2,18 +2,18 @@ import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+import xacro
 
 def generate_launch_description():
-    description_pkg_share = launch_ros.substitutions.FindPackageShare(package='ghost_description').find('ghost_description')
-    ros_pkg_share = launch_ros.substitutions.FindPackageShare(package='ghost_ros').find('ghost_ros')
-    default_model_path = os.path.join(description_pkg_share, 'urdf/ghost1.urdf')
-    default_rviz_config_path = os.path.join(ros_pkg_share, 'rviz/urdf_config.rviz')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='ghost_over_under').find('ghost_over_under')
+    xacro_path = os.path.join(pkg_share, 'urdf/ghost_15.xacro')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         name = "robot_state_publisher",
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
+        parameters=[{'robot_description': Command(['xacro ', xacro_path])}]
     )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
@@ -34,8 +34,8 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        # joint_state_publisher_gui_node,
-        # joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        joint_state_publisher_node,
         robot_state_publisher_node,
         rviz_node
     ])
