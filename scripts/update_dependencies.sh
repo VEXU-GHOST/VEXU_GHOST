cf224478a997ae04416b9782a7945651b8219508
MELISSA CRUZ <melissajecruz@utexas.edu>
2024-02-01 17:28:47 -0600

    Changes dep to add pj and pjros

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -4,7 +4,7 @@ cd ~/VEXU_GHOST
 echo
 echo "--------------- Non-ROS Dependencies ---------------"
 sudo apt-get install -y libgoogle-glog-dev cmake python3-colcon-common-extensions gfortran-10
-sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev python3-rosdep2 apt-rdepends
+sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev python3-rosdep2 apt-rdepends xacro ros-humble-plotjuggler ros-humble-plotjuggler-ros
 pip install colcon-lint
 
 echo
