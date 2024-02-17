958623f451e4ea189534daa6b5356eac8a3f60f5
xwilson03 <xanderwilson2014@gmail.com>
2024-02-17 03:37:26 -0600

    adds test file

---
+++ 01_Libraries/ghost_planners/test/test_robot_trajectory.cpp
@@ -0,0 +1,17 @@
+#include <ghost_planners/robot_trajectory.hpp>
+#include <gtest/gtest.h>
+
+using ghost_planners::RobotTrajectory;
+
+class RobotTrajectoryTestFixture : public ::testing::Test {
+public:
+	RobotTrajectoryTestFixture(){
+	}
+
+	void SetUp() override {
+	}
+};
+
+TEST_F(RobotTrajectoryTestFixture, testConstructors){
+	EXPECT_NO_THROW(auto traj = RobotTrajectory());
+}
