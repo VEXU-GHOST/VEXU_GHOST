d6d59065b7917657fa2d80d6a5c5b99069ac5fbe
xwilson03 <xanderwilson2014@gmail.com>
2024-05-21 17:06:35 -0500

    updates pros build script exit handling

--- scripts/pros_build.sh
+++ scripts/pros_build.sh
@@ -5,28 +5,25 @@ if [[ $(pros --version) ]] 2> /dev/null; then
     echo -------------------------------------------------------
 	echo ----------- Updating Project Symbolic Links -----------
 	echo -------------------------------------------------------
-    bash scripts/update_symlinks.sh
+    bash scripts/update_symlinks.sh || exit -1
     
     echo 
-    ./build/ghost_v5_interfaces/generate_pros_header robots.yaml
+    ./build/ghost_v5_interfaces/generate_pros_header robots.yaml || exit -1
 
-    RETURN=$?
-    if [ $RETURN -eq 0 ]; then
-        cd 02_V5/ghost_pros
+    cd 02_V5/ghost_pros
 
-        echo
-        echo -------------------------------------------------------
-        echo ---------------- Cleaning PROS Project ----------------
-        echo -------------------------------------------------------
-        make clean
+    echo
+    echo -------------------------------------------------------
+    echo ---------------- Cleaning PROS Project ----------------
+    echo -------------------------------------------------------
+    make clean || exit -1
 
-        echo 
-        echo -------------------------------------------------------
-        echo ---------------- Building PROS Project ----------------
-        echo -------------------------------------------------------
-        echo
-        pros make
-    fi
+    echo 
+    echo -------------------------------------------------------
+    echo ---------------- Building PROS Project ----------------
+    echo -------------------------------------------------------
+    echo
+    pros make || exit -1
 else
     echo "ERROR: PROS is not installed!"
 fi
