ad2644db421d49a97371ae67154ae7cd1b63e60a
Maxx Wilson <jessemaxxwilson@utexas.edu>
2024-05-22 19:28:51 -0500

    adds licenses to 11_Robots

--- 10_Examples/ghost_examples/scripts/cubic_trajectory.py
+++ 10_Examples/ghost_examples/scripts/cubic_trajectory.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # These would be determined by solving the coefficient matrix from the initial and final position/velocity/time
-a = -8/27
-b = 20/9
-c = -32/9
-d = 71/27
+a = -8 / 27
+b = 20 / 9
+c = -32 / 9
+d = 71 / 27
+
 
 def position(t):
-    return a*t**3 + b*t**2 + c*t + d
+    return a * t**3 + b * t**2 + c * t + d
+
 
 def velocity(t):
-    return 3*a*t**2 + 2*b*t + c
+    return 3 * a * t**2 + 2 * b * t + c
+
 
 def accel(t):
-    return 6*a*t + 2*b
+    return 6 * a * t + 2 * b
+
 
 t = np.arange(1, 4, 0.01)
 
