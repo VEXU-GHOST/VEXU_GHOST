61879fcaeeef381db35bb7a5c649390248270f38
xwilson03 <xanderwilson2014@gmail.com>
2024-05-23 05:04:28 -0500

    im all in

--- scripts/setup_submodules.sh
+++ scripts/setup_submodules.sh
@@ -13,77 +13,17 @@ git submodule update --recursive
 # Get processor architecture to determine proper .deb source
 arch=$(uname -p)
 
+cd $VEXU_HOME
+
+
 # Build matplotlib-cpp
 echo "--------------- MATPLOTLIB_CPP ---------------"
-if [ ! -d "${VEXU_HOME}/09_External/matplotlib-cpp/build" ];
-then
-	cd $VEXU_HOME/09_External/matplotlib-cpp
-	mkdir build && cd build
-
-	cmake ..          || exit -1
-	make              || exit -1
-	sudo make install || exit -1
-
-	cd ../..
-else
-	echo "Build already exists"
-fi
-echo
-echo
-
-# Build Mumps
-echo "--------------- MUMPS ---------------"
-cd $VEXU_HOME/09_External/ThirdParty-Mumps
-
-sudo apt install gfortran-10 liblapack-dev pkg-config --install-recommends -y || exit -1
-sudo apt install swig -y                                                      || exit -1
-
-export FC=$(which gfortran-10)
-
-./configure       || exit -1
-make              || exit -1
-sudo make install || exit -1
-
-cd ..
-
-echo
-echo
-
-# Build IPOPT
-echo "--------------- IPOPT ---------------"
-cd Ipopt
-
-./configure       || exit -1
-make              || exit -1
-make test         || exit -1
-sudo make install || exit -1
-
-echo
-echo
-
-# Build Casadi
-echo "--------------- CASADI ---------------"
-# if [ ! -d "${VEXU_HOME}/09_External/casadi/build" ];
-# then
-# 	cd $VEXU_HOME/09_External/casadi
-# 	mkdir build && cd build
-
-# 	cmake -DWITH_PYTHON=ON -DWITH_IPOPT=ON -DWITH_OPENMP=ON -DWITH_THREAD=ON .. || exit -1
-# 	make                                                                        || exit -1
-# 	sudo make install                                                           || exit -1
-
-# 	cd ../..
-# else
-#         echo "Build already exists"
-# fi
-
-cd $VEXU_HOME
 
 case $arch in
 
     'x86_64')
-        sudo wget https://github.com/VEXU-GHOST/ghost_dependencies/raw/22db13647a74e7911c249b5762ea3c6d12893aa4/deb/ghost-casadi-86.deb || exit -1
-        sudo dpkg -i ghost-casadi-86.deb || exit -1
+        sudo wget https://github.com/VEXU-GHOST/ghost_dependencies/raw/main/deb/ghost-matplotlibcpp-86.deb || exit -1
+        sudo dpkg -i ghost-matplotlibcpp-86.deb || exit -1
         ;;
     
     'aarch64')
@@ -97,6 +37,82 @@ case $arch in
         ;;
 esac
 
+echo
+echo
+
+# Build Mumps
+echo "--------------- MUMPS ---------------"
+
+sudo apt install gfortran-10 liblapack-dev pkg-config --install-recommends -y || exit -1
+sudo apt install swig -y                                                      || exit -1
+
+export FC=$(which gfortran-10)
+
+case $arch in
+
+    'x86_64')
+        sudo wget https://github.com/VEXU-GHOST/ghost_dependencies/raw/main/deb/ghost-mumps-86.deb || exit -1
+        sudo dpkg -i ghost-mumps-86.deb || exit -1
+        ;;
+    
+    'aarch64')
+        echo "TODO(xander): add arm debian link here"
+        echo "TODO(xander): add arm debian installation here"
+        ;;
+
+    *)
+        echo "Failure: Unexpected processure architecture."
+        exit -1
+        ;;
+esac
+
+echo
+echo
+
+# Build IPOPT
+echo "--------------- IPOPT ---------------"
+
+case $arch in
+
+    'x86_64')
+        sudo wget https://github.com/VEXU-GHOST/ghost_dependencies/raw/main/deb/ghost-ipopt-86.deb || exit -1
+        sudo dpkg -i ghost-ipopt-86.deb || exit -1
+        ;;
+    
+    'aarch64')
+        echo "TODO(xander): add arm debian link here"
+        echo "TODO(xander): add arm debian installation here"
+        ;;
+
+    *)
+        echo "Failure: Unexpected processure architecture."
+        exit -1
+        ;;
+esac
+
+echo
+echo
+
+# Build Casadi
+echo "--------------- CASADI ---------------"
+
+case $arch in
+
+    'x86_64')
+        sudo wget https://github.com/VEXU-GHOST/ghost_dependencies/raw/main/deb/ghost-casadi-86.deb || exit -1
+        sudo dpkg -i ghost-casadi-86.deb || exit -1
+        ;;
+    
+    'aarch64')
+        echo "TODO(xander): add arm debian link here"
+        echo "TODO(xander): add arm debian installation here"
+        ;;
+
+    *)
+        echo "Failure: Unexpected processure architecture."
+        exit -1
+        ;;
+esac
 
 echo
 echo
