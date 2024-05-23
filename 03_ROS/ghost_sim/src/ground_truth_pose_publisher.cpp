51aa50956e63f8d3f301f5a0b52e3b2c9cb3ccb1
Maxx Wilson <jessemaxxwilson@utexas.edu>
2024-05-22 19:21:01 -0500

    adds licenses to 03_ROS

--- 03_ROS/ghost_sim/src/ground_truth_pose_publisher.cpp
+++ 03_ROS/ghost_sim/src/ground_truth_pose_publisher.cpp
@@ -1,11 +1,24 @@
 /*
- * Filename: ground_truth_pose
- * Created Date: Monday October 24th 2022
- * Author: Maxx Wilson
- * Author Email: JesseMaxxWilson@utexas.edu
- *
- * Last Modified: Monday October 24th 2022 2:24:18 pm
- * Modified By: Maxx Wilson
+ *   Copyright (c) 2024 Maxx Wilson
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
  */
 
 #include <chrono>
