1c5a726a79c055a0aba2b4fcc6fd77fe305fa2a7
Zaara <zbilal6135@gmail.com>
2024-05-30 14:12:59 -0500

    changes to comments

--- 04_Sim/ghost_sim_examples/launch/rviz.launch.py
+++ 04_Sim/ghost_sim_examples/launch/rviz.launch.py
@@ -30,8 +30,7 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        joint_state_publisher_gui_node,
-       
+        joint_state_publisher_gui_node,       
         robot_state_publisher_node,
         rviz_node
     ])
--- 04_Sim/ghost_sim_examples/launch/start_sim.launch.py
+++ 04_Sim/ghost_sim_examples/launch/start_sim.launch.py
@@ -35,35 +35,15 @@ def launch_setup(context, *args, **kwargs):
         output='screen',
         parameters=[{'use_sim_time': False}, {"robot_description": doc}])
 
-    # Joystick (Only launched if joystick CLI arg is set to True)
-    # joy_launch_description = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(
-    #             ghost_sim_share_dir,
-    #             "launch",
-    #             "joystick.launch.py"
-    #         )
-    #     ),
-    #     condition=launch.conditions.IfCondition(LaunchConfiguration("use_joy")),
-    #     launch_arguments={
-    #         'channel_id': LaunchConfiguration("channel_id"),
-    #     }.items()
-    # )
-
     return [gazebo_ros, robot_state_publisher]
 
 
 def generate_launch_description():
     # Load relevant filepaths
     gazebo_ros_share_dir = get_package_share_directory('gazebo_ros')
-    # ghost_ros_share_dir = get_package_share_directory('ghost_ros')
     ghost_sim_share_dir = get_package_share_directory('ghost_sim_examples')
-
     home_dir = os.path.expanduser('~')
-    # ghost_ros_base_dir = os.path.join(home_dir, "VEXU_GHOST", "03_ROS", "ghost_ros")
-
     world_file = os.path.join(ghost_sim_share_dir, "world", "default.world")
-    # rviz_config_path = os.path.join(ghost_ros_share_dir, 'rviz/urdf_config.rviz')
 
     # Simulator (Doesn't launch Simulator GUI by default, use CLI Arg "sim_gui" for debugging)
     simulation = IncludeLaunchDescription(
@@ -78,41 +58,11 @@ def generate_launch_description():
             }.items()
     )
 
-
-
-    # rviz_node = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     arguments=['-d', rviz_config_path],
-    # )
-
-    # estimator_node = Node(
-    #     package='ghost_ros',
-    #     executable='ghost_estimator_node',
-    #     name='ghost_estimator_node',
-    #     output='screen',
-    #     parameters=[ghost_ros_base_dir + "/config/ghost_estimator_config.yaml"]
-    # )
-
-    # state_machine_node = Node(
-    #     package='ghost_ros',
-    #     executable='robot_state_machine_node',
-    #     name='ghost_state_machine',
-    #     output='screen',
-    #     parameters=[ghost_ros_base_dir + "/config/ghost_state_machine_config.yaml"]
-    # )
-
     return LaunchDescription([
         DeclareLaunchArgument(name='use_joy', default_value='true'),
         DeclareLaunchArgument(name='channel_id', default_value='1'),
         DeclareLaunchArgument('sim_gui', default_value='true'),
         DeclareLaunchArgument('verbose', default_value='true'),
         simulation,
-        # ground_truth_publisher,
-        # rviz_node,
-        # estimator_node,
-        # state_machine_node,
-        # v5_actuator_cmd_publisher,
         OpaqueFunction(function = launch_setup)
     ])
