93c6fef1c89afc6988721e5fce3dd5427d4f109f
xwilson03 <xanderwilson2014@gmail.com>
2024-06-04 07:44:50 -0500

    adds digital actuator interface

---
+++ 01_Libraries/ghost_v5_interfaces/include/ghost_v5_interfaces/devices/digital_actuator_device_interface.hpp
@@ -0,0 +1,102 @@
+/*
+ *   Copyright (c) 2024 Maxx Wilson, Xander Wilson
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+#pragma once
+
+#include <cstring>
+#include "ghost_v5_interfaces/devices/device_interfaces.hpp"
+
+namespace ghost_v5_interfaces
+{
+
+namespace devices
+{
+
+class DigitalActuatorDeviceData : public DeviceData
+{
+public:
+  DigitalActuatorDeviceData(std::string name)
+  : DeviceData(name, device_type_e::DIGITAL_ACTUATOR)
+  {
+  }
+
+  int getActuatorPacketSize() const override
+  {
+    return 1;
+  }
+
+  int getSensorPacketSize() const override
+  {
+    return 0;
+  }
+
+  bool value;
+
+  void update(std::shared_ptr<DeviceData> data_ptr) override
+  {
+    auto digital_data_ptr = data_ptr->as<DigitalActuatorDeviceData>();
+    value = digital_data_ptr->value;
+  }
+
+  std::shared_ptr<DeviceBase> clone() const override
+  {
+    return std::make_shared<DigitalActuatorDeviceData>(*this);
+  }
+
+  bool operator==(const DeviceBase & rhs) const override
+  {
+    const DigitalActuatorDeviceData * d_rhs = dynamic_cast<const DigitalActuatorDeviceData *>(&rhs);
+    return (d_rhs != nullptr && value == d_rhs->value);
+  }
+
+  std::vector<unsigned char> serialize(hardware_type_e hardware_type) const override
+  {
+    return {(unsigned char) ((hardware_type == COPROCESSOR && value) ? 0x1 : 0x0)};
+  }
+
+  void deserialize(const std::vector<unsigned char> & msg, hardware_type_e hardware_type) override
+  {
+    if (hardware_type == COPROCESSOR) return;
+    value = (msg.data()[0] == 0x1);
+  }
+};
+
+class DigitalActuatorDeviceConfig : public DeviceConfig
+{
+public:
+  std::shared_ptr<DeviceBase> clone() const override
+  {
+    return std::make_shared<DigitalActuatorDeviceConfig>(*this);
+  }
+
+  bool operator==(const DeviceBase & rhs) const override
+  {
+    const DigitalActuatorDeviceConfig * d_rhs = dynamic_cast<const DigitalActuatorDeviceConfig *>(&rhs);
+    return (d_rhs != nullptr) && (port == d_rhs->port) && (name == d_rhs->name) &&
+           (type == d_rhs->type);
+  }
+};
+
+} // namespace devices
+
+} // namespace ghost_v5_interfaces
