d89a1a4c078db1be90d5736a347af8db23775356
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2024-09-30 16:06:25 -0500

    autologin script for jetson (still kinda slow, do we want to boot from root???)

--- scripts/hardware/service.sh
+++ scripts/hardware/service.sh
@@ -33,6 +33,19 @@ case "$1" in
         systemctl --user link $DIR/*service
         systemctl --user enable --now $DIR/*service
         ;;
+    "autologin")
+	sudo mkdir -p /etc/systemd/system/serial-getty@ttyTCU0.service.d
+	sudo tee /etc/systemd/system/serial-getty@ttyTCU0.service.d/override.conf <<EOF > /dev/null
+[Service]
+# The '-o' option value tells agetty to replace 'login' arguments with an
+# option to preserve environment (-p), followed by '--' for safety, and then
+# the entered username.
+ExecStart=
+ExecStart=-/sbin/agetty --autologin ghost --keep-baud 115200,57600,38400,9600 %I $TERM
+EOF
+	sudo systemctl daemon-reload
+	sudo systemctl restart serial-getty@ttyTCU0.service
+	;;
     *)
         echo "Usage: service.sh [restart/stop/kill/shutdown/install]"
         ;;
