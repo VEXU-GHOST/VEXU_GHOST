df258df8f75f4e4431b7fc5ad6e058270386f4a2
Jett Vanbrocklin <jettvanbrocklin@utexas.edu>
2024-09-30 15:17:13 -0500

    Jett's Onboarding Pull Request, Take 2

--- 10_Examples/jett-onboarding/src/publisher_member_function.cpp
+++ 10_Examples/jett-onboarding/src/publisher_member_function.cpp
@@ -31,13 +31,13 @@ public:
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-    500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
 private:
   void timer_callback()
   {
-    auto message = int[];
+    auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
