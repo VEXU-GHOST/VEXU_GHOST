900418f906c54ef3e879be70491184593e01cf68
xwilson03 <xanderwilson2014@gmail.com>
2025-02-01 15:30:16 -0600

    simplifies diff

--- 01_Libraries/ghost_v5_interfaces/include/ghost_v5_interfaces/devices/device_interfaces.hpp
+++ 01_Libraries/ghost_v5_interfaces/include/ghost_v5_interfaces/devices/device_interfaces.hpp
@@ -166,7 +166,7 @@ public:
   /**
    * @brief Converts device data to byte stream.
    *
-   * @param hardware_type current hardware type
+   * @param to_v5	set to true when the data is going from coprocessor -> V5 Brain
    * @return std::vector<unsigned char> byte stream
    */
   virtual std::vector<unsigned char> serialize(hardware_type_e hardware_type) const = 0;
@@ -175,7 +175,7 @@ public:
    * @brief Updates device data from byte stream.
    *
    * @param data byte stream as unsigned char vector
-   * @param hardware_type current hardware type
+   * @param from_coprocessor set to true when the data is going from coprocesspr -> V5 Brain
    */
   virtual void deserialize(
     const std::vector<unsigned char> & data,
