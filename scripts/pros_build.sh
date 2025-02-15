c91719cd5b69c205e084012f54e52d57388269f7
xwilson03 <xanderwilson2014@gmail.com>
2025-02-15 10:25:45 -0600

    make pros build fast

--- scripts/pros_build.sh
+++ scripts/pros_build.sh
@@ -23,7 +23,8 @@ if [[ $(pros --version) ]] 2> /dev/null; then
     echo ---------------- Building PROS Project ----------------
     echo -------------------------------------------------------
     echo
-    pros make || exit -1
+    #pros make || exit -1
+    make -j4 || exit -1
 else
     echo "ERROR: PROS is not installed!"
 fi
