a9967ac1c3e431dd1efec2a65edbcb1dd8017365
JakeWendling <jakewendling57@gmail.com>
2025-02-16 14:05:46 -0600

    changes formatting in ros yaml and removes comment from setworldpose

--- 11_Robots/ghost_tank/include/ghost_tank/tank_model.hpp
+++ 11_Robots/ghost_tank/include/ghost_tank/tank_model.hpp
@@ -106,14 +106,6 @@ public:
     m_world_pose.x() = x;
     m_world_pose.y() = y;
     m_world_pose.z() = theta;
-    // geometry_msgs::msg::PoseWithCovarianceStamped msg{};
-    // msg.header.stamp = node_ptr_->get_clock()->now();
-    // msg.pose.pose.position.x = x;
-    // msg.pose.pose.position.y = y;
-    // ghost_util::yawToQuaternionDeg(
-    //   theta, msg.pose.pose.orientation.w, msg.pose.pose.orientation.x,
-    //   msg.pose.pose.orientation.y, msg.pose.pose.orientation.z);
-    // m_particle_filter_set_pose_publisher->publish(msg);
   }
 
   double getWorldAngleDeg() const
