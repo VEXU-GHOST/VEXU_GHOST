752b80d5b4e78debe4f053fd7045ecff8d72cdce
MELISSA CRUZ <melissajecruz@utexas.edu>
2025-02-17 13:57:00 -0600

    fixes warnings from old setuptools version fixes warning in particle filter node removes -r flag and checks directly for pros adds pip and setuptools version to dependency update disables failing unit test :/

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,9 +46,11 @@ then
     colcon build --symlink-install --packages-skip ${skip[@]} ghost_sim ghost_sim_examples ghost_viz plotjuggler plotjuggler_ros --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON || exit -1
 fi
 
-if [ "$1" != "-r" ];
+if ! command -v pros 2>&1 >/dev/null
 then
-  cd $VEXU_HOME
-  echo
-  bash scripts/pros_upload.sh
+    exit;
 fi
+
+cd $VEXU_HOME
+echo
+bash scripts/pros_upload.sh
