5977520646ed51f7fb0846c3a11108cf85748179
Maxx Wilson <JesseMaxxWilson@utexas.edu>
2025-02-17 17:58:14 -0600

    Feature/cleans up tank robot plugin (#121)
    # PR Summary
    PR Link: [Link](https://github.com/VEXU-GHOST/VEXU_GHOST/pull/121)
    
    ### Description
    Large refactoring of tank robot plugin for readability. Confirmed tests
    on hardware.
    
    ### Reviewers
    Tag reviewers.
    
    - Required: @JakeWendling 
      
    - Optional: @melissajcruz @karmanyaahm 
    
    ---
    ### Changelog
    - Split up init and teleop into submethods to make it way more readable.
    - Reorganized by broad categories of ROS, autonomy, estimation, tank
    model, etc
    
    ### Reviewer Guide
    This is the most important part!
    - Mostly moving things around. There is more to go tbh.
    
    ### Testing
    #### Manual
    - Test on Omega Jerry
    
    ---------
    
    Co-authored-by: JakeWendling <jakewendling57@gmail.com>
    Co-authored-by: Karmanyaah Malhotra <karmanyaah.gh@malhotra.cc>
    Co-authored-by: xwilson03 <xanderwilson2014@gmail.com>
    Co-authored-by: linh tran <lilytran0605@gmail.com>
    Co-authored-by: Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
    Co-authored-by: MELISSA CRUZ <melissajecruz@utexas.edu>

--- 11_Robots/ghost_tank/include/ghost_tank/tank_robot_plugin.hpp
+++ 11_Robots/ghost_tank/include/ghost_tank/tank_robot_plugin.hpp
@@ -60,14 +60,34 @@ public:
   void onNewSensorData() override;
 
 protected:
-  // Publishers
-  void readPathFromFile(const std::string& filename);
-  void publishVisualization();
-  void publishOdometry();
+  // Construction
+  void populateMotorNames();
+  void populateDigitalIONames();
+
+  // Initialization
+  void initROSComms();
+  void initEstimation();
+  void initTankModel();
+  void initAutonomy();
+
+  // onNewSensorData
+  void publishIMUData();
+  void updateAndPublishOdometry();
   void publishBaseTwist();
   void publishTrajectoryVisualization();
+
+  // Teleop
+  bool runAutonFromDriver(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data, double current_time);
+  void toggleBagRecorder(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data);
+  void updateIntake(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data);
+  void updateClamp(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data);
+  void updateDrivetrain(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data);
+  void updateBite(std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData> joy_data);
+
+  void readPathFromFile(const std::string & filename);
   void resetPose(double x, double y, double theta);
 
+ 
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr m_odom_pub;
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr m_joint_state_pub;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr m_tank_viz_pub;
@@ -75,7 +95,6 @@ protected:
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr m_imu_sub;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr m_base_twist_cmd_pub;
 
-  // rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr m_cur_pos_pub;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr m_des_twist_pub;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr m_cur_twist_pub;
   rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr m_des_pos_pub;
@@ -106,8 +125,7 @@ protected:
   rclcpp::Client<ghost_msgs::srv::StartRecorder>::SharedPtr m_start_recorder_client;
   rclcpp::Client<ghost_msgs::srv::StopRecorder>::SharedPtr m_stop_recorder_client;
 
-  // tank Model
-  void updateDrivetrainMotors();
+  // Tank Model
   std::shared_ptr<TankModel> m_tank_model_ptr;
 
   // Autonomy
@@ -152,6 +170,9 @@ protected:
   double m_init_world_y = 0.0;
   double m_init_world_theta = 0.0;
   bool m_use_backup_estimator = false;
+
+  bool m_clamp_closed{false};
+  bool m_bite_closed{false};
 
   // Digital IO
   std::vector<bool> m_digital_io;
@@ -210,6 +231,8 @@ protected:
   std::vector<std::string> m_right_drive_motor_names;
   std::vector<std::string> m_left_drive_motor_names;
   std::vector<std::string> m_all_motor_names;
+
+  std::unordered_map<std::string, int> digital_io_port_map;
 };
 
 } // namespace ghost_tank
