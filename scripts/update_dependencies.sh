9a40270a261b055debd6bbfd7457e168b61a354e
Omega Jerry <OmegaJerry@ras.ece.utexas.edu>
2025-02-21 23:19:53 -0600

    make dependency install order more robust for jetson

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -14,6 +14,8 @@ echo "--------------- Non-ROS Dependencies ---------------"
 sudo apt-get install -y libgoogle-glog-dev cmake python3-colcon-common-extensions gfortran-10 libi2c-dev libi2c0 ccache || exit -1
 sudo apt-get install -y python3-pip libgtest-dev libgoogle-glog-dev python3-rosdep2 apt-rdepends ros-humble-xacro sox libsox-fmt-mp3 || exit -1
 pip install colcon-lint || exit -1
+python3 -m pip install --upgrade pip
+pip install setuptools==61 piper-tts==1.2.0
 
 echo
 echo "--------------- ROSDEP Init ---------------"
@@ -27,5 +29,3 @@ echo
 echo "--------------- ROSDEP Install ---------------"
 rosdep install --from-paths . --ignore-src -r -y || exit -1
 
-python3 -m pip install --upgrade pip
-pip install setuptools==61 piper-tts==1.2.0
