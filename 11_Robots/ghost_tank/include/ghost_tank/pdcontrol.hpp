700c4302a84abf56c045e0fbf7f3175bb07ba9d3
Maxx Wilson <jessemaxxwilson@utexas.edu>
2025-02-28 22:57:11 -0600

    QoL in tank pd

--- 11_Robots/ghost_tank/include/ghost_tank/pdcontrol.hpp
+++ 11_Robots/ghost_tank/include/ghost_tank/pdcontrol.hpp
@@ -11,12 +11,13 @@ class PDControl
 
 public:
   //constructor
-  PDControl(float kp_xy_,
+  PDControl(
+    float kp_xy_,
     float kd_xy_,
     float kp_theta_,
     float kd_theta_);
-  Eigen::Vector2d tank_pid(Eigen::Vector3d cur_pos, Eigen::Vector3d cur_twist, Eigen::Vector3d & carrot_pos, Eigen::Vector3d final_pos, bool backwards);
-  Eigen::Vector2d theta_pid(Eigen::Vector3d cur_pos, Eigen::Vector3d cur_twist, Eigen::Vector3d end_pos);
+  Eigen::Vector2d tank_pid(const Eigen::Vector3d & cur_pos, const Eigen::Vector3d & cur_twist, const Eigen::Vector3d & carrot_pos, const Eigen::Vector3d & final_pos, bool backwards);
+  Eigen::Vector2d theta_pid(const Eigen::Vector3d & cur_pos, const Eigen::Vector3d & cur_twist, const Eigen::Vector3d & end_pos);
 
 private:
   float kp_xy_;
