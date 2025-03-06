6e479dd06e4aa22a177004c044efb3c9e24a6784
Karmanyaah Malhotra <karmanyaah.gh@malhotra.cc>
2025-03-06 00:56:45 -0500

    Feature/power button (#131)
    i am not writing a changelog. disable gui on boot unless you run `sudo
    systemctl start gdm3`. GDM takes over the power button and breaks it,
    since we run headless its useless most of the time, so i'm removing it.

--- scripts/hardware/service.sh
+++ scripts/hardware/service.sh
@@ -45,6 +45,7 @@ ExecStart=-/sbin/agetty --autologin ghost --keep-baud 115200,57600,38400,9600 %I
 EOF
 	sudo systemctl daemon-reload
 	sudo systemctl restart serial-getty@ttyTCU0.service
+	sudo systemctl disable --now gdm3
 	echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nopasswd-$USER
 
 	;;
