bda64b588c19ab0f9b6e093a6d34ea58e56a181f
Annie <Annie.2005@outlook.com>
2025-04-06 17:17:54 -0500

    meow

--- 11_Robots/ghost_tank/src/tank_robot_plugin.cpp
+++ 11_Robots/ghost_tank/src/tank_robot_plugin.cpp
@@ -32,116 +32,6 @@ using ghost_planners::RobotTrajectory;
 using ghost_ros_interfaces::msg_helpers::fromROSMsg;
 using std::placeholders::_1;
 
-namespace ghost_example_robot
-{
-
-GhostExampleRobot::GhostExampleRobot()
-{
-}
-
-void GhostExampleRobot::initialize()
-{
-  // Only called once when program starts!
-  std::cout << "initialize" << std::endl;
-}
-
-void GhostExampleRobot::onNewSensorData()
-{
-  // Optional, called before disabled/autonomous/teleop when new data arrives.
-  // std::cout << "onNewSensorData" << std::endl;
-}
-
-void GhostExampleRobot::disabled()
-{
-  std::cout << "disabled" << std::endl;
-}
-
-void GhostExampleRobot::autonomous(double current_time)
-{
-  std::cout << "Autonomous" << current_time << std::endl;
-}
-
-void GhostExampleRobot::teleop(double current_time)
-{
-  static int loop_count = 0;
-  if (loop_count++ % 100 == 0) {
-    std::cout << "Teleop " << current_time << std::endl;
-  }
-
-  auto joy_data = rhi_ptr_->getMainJoystickData();
-
-  if (joy_data->btn_a) {
-    std::cout << "Button A!" << std::endl;
-  } else if (joy_data->btn_b) {
-    std::cout << "Button B!" << std::endl;
-  } else if (joy_data->btn_x) {
-    std::cout << "Button X!" << std::endl;
-  } else if (joy_data->btn_y) {
-    std::cout << "Button Y!" << std::endl;
-  } else if (joy_data->btn_u) {
-    std::cout << "Button U!" << std::endl;
-  } else if (joy_data->btn_d) {
-    std::cout << "Button D!" << std::endl;
-  } else if (joy_data->btn_l) {
-    std::cout << "Button L!" << std::endl;
-  } else if (joy_data->btn_r) {
-    std::cout << "Button R!" << std::endl;
-  } else if (joy_data->btn_l1) {
-    std::cout << "Button L1!" << std::endl;
-  } else if (joy_data->btn_l2) {
-    std::cout << "Button L2!" << std::endl;
-  }
-
-  // Print joystick data!
-  if (joy_data->btn_r1) {
-    // Left joystick up-down axis is "left_y", left-right axis is "left_x"
-    // Right joystick up-down axis is "right_y", left-right axis is "right_x"
-    std::cout << "Left X: " << joy_data->left_x << std::endl;
-    std::cout << "Left Y: " << joy_data->left_y << std::endl;
-    std::cout << "Right X: " << joy_data->right_x << std::endl;
-    std::cout << "Right Y: " << joy_data->right_y << std::endl;
-    std::cout << std::endl;
-  }
-
-  // While holding button R2, send motor commands based on joystick values
-  if (joy_data->btn_r2) {
-    // Joysticks go from -127 to 127, but motors take a value from -1.0 to 1.0.
-    double left_wheel_power = joy_data->left_y / 127.0;
-    double right_wheel_power = joy_data->right_y / 127.0;
-
-    // setMotorVoltageCommandPercent maps -1.0 <-> 1.0 to -12000 <-> 12000 milliVolts behind the scenes.
-    rhi_ptr_->setMotorVoltageCommandPercent("left_motor", left_wheel_power);
-    rhi_ptr_->setMotorVoltageCommandPercent("right_motor", right_wheel_power);
-
-    // Each motor has a current limit that defaults to zero.
-    // This is so we can carefully allocate battery power between systems.
-    // If we don't set these, the motors will be extremely weak, if they move at all.
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("left_motor", 2500.0);
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("right_motor", 2500.0);
-
-    // Now we can get motor data and print it.
-    double left_position = rhi_ptr_->getMotorPosition("left_motor");
-    double right_position = rhi_ptr_->getMotorPosition("right_motor");
-
-    // These are in degrees. Units and other data can be configured in example_hardware_config.yaml.
-    std::cout << "Left Motor: " << left_position << " deg" << std::endl;
-    std::cout << "Right Motor: " << right_position << " deg" << std::endl;
-    std::cout << std::endl;
-  } else {
-    // Don't forget to turn motors off!
-    rhi_ptr_->setMotorVoltageCommandPercent("left_motor", 0.0);
-    rhi_ptr_->setMotorVoltageCommandPercent("right_motor", 0.0);
-
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("left_motor", 0.0);
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("right_motor", 0.0);
-  }
-}
-} // namespace ghost_example_robot
-
-PLUGINLIB_EXPORT_CLASS(
-  ghost_example_robot::GhostExampleRobot,
-  ghost_ros_interfaces::V5RobotBase)
-
 namespace ghost_tank
 {
 
@@ -427,88 +317,51 @@ void TankRobotPlugin::autonomous(double current_time)
 
 void TankRobotPlugin::teleop(double current_time)
 {
-
   auto joy_data = rhi_ptr_->getMainJoystickData();
   // std::cout << "Teleop: " << current_time << std::endl;
 
+  if (joy_data->btn_u) {
+    if (!m_auton_button_pressed) {
+      m_auton_button_pressed = true;
+      m_is_first_auton_loop = true;
+      m_auton_start_time = current_time;
+      m_auton_index = 0;
+    }
+    autonomous(current_time - m_auton_start_time);
+  } else {
+    m_auton_button_pressed = false;
 
-  if(joy_data->btn_a){
-    double left_wheel_power = joy_data->left_y / 127.0;
-    double right_wheel_power = joy_data->right_y / 127.0;
+    // Toggle Bag Recorder
+    if (joy_data->btn_y && !m_recording_btn_pressed) {
+      m_recording_btn_pressed = true;
 
-    // setMotorVoltageCommandPercent maps -1.0 <-> 1.0 to -12000 <-> 12000 milliVolts behind the scenes.
-    rhi_ptr_->setMotorVoltageCommandPercent("left_motor", left_wheel_power);
-    rhi_ptr_->setMotorVoltageCommandPercent("right_motor", right_wheel_power);
+      if (!m_recording) {
+        auto req = std::make_shared<ghost_msgs::srv::StartRecorder::Request>();
+        m_start_recorder_client->async_send_request(req);
+      } else {
+        auto req = std::make_shared<ghost_msgs::srv::StopRecorder::Request>();
+        m_stop_recorder_client->async_send_request(req);
+      }
 
-    // Each motor has a current limit that defaults to zero.
-    // This is so we can carefully allocate battery power between systems.
-    // If we don't set these, the motors will be extremely weak, if they move at all.
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("left_motor", 2500.0);
-    rhi_ptr_->setMotorCurrentLimitMilliAmps("right_motor", 2500.0);
+      m_recording = !m_recording;
+    } else if (!joy_data->btn_y) {
+      m_recording_btn_pressed = false;
+    }
 
-    // Now we can get motor data and print it.
-    double left_position = rhi_ptr_->getMotorPosition("left_motor");
-    double right_position = rhi_ptr_->getMotorPosition("right_motor");
+    m_tank_model_ptr->driveCommandJoystick(
+      joy_data->left_y, joy_data->right_x, 0.05);
 
-    // These are in degrees. Units and other data can be configured in example_hardware_config.yaml.
-    std::cout << "Left Motor: " << left_position << " deg" << std::endl;
-    std::cout << "Right Motor: " << right_position << " deg" << std::endl;
-    std::cout << std::endl;
-  }
+    double intake_power = 0;
+    if (joy_data->btn_r2) {
+      intake_power = 1.0;
+    } else if (joy_data->btn_r1) {
+      intake_power = -1.0;
+    } else {
+      intake_power = 0.0;
+    }
 
-  if(joy_data->btn_b){
-
-  double forward_vel = joy_data->left_y / 127.0;
-  double angular_vel = joy_data->right_x / 127.0;
-
-  double threshold = 0.05;
-  forward_vel = (std::fabs(forward_vel) < threshold) ? 0.0 : forward_vel;
-  angular_vel = (std::fabs(angular_vel) < threshold) ? 0.0 : angular_vel;
-
-  double left_cmd = forward_vel + angular_vel;
-  double right_cmd = forward_vel - angular_vel;
-
-  // this is from ghost_high_stakes/config/robot_hardware_config_tank.yaml
-  std::vector<std::string> motor_list = {
-    "drive_ltr",
-    "drive_lbr",
-    "drive_ltf",
-    "drive_lbf",
-    "drive_lttf",
-    "indexer_right",
-    "indexer_left",
-    "drive_rttf",
-    "drive_rtr",
-    "drive_rbr",
-    "drive_rtf",
-    "drive_rbf"
-  };
-  
-  for (const auto motor_name: motor_list){
-    rhi_ptr_->setMotorCurrentLimitMilliAmps(motor_name, 2500);
-  }
-
-  for (int i = 0; i < 5; i++){
-    rhi_ptr_->setMotorVoltageCommandPercent(motor_list[i], left_cmd);
-  }
-
-  for (int i = 7; i < 12; i++){
-    rhi_ptr_->setMotorVoltageCommandPercent(motor_list[i], right_cmd);
-  }
-
-  double intake_power = 0;
-  if(joy_data->btn_r2){
-    intake_power = 1.0;
-  }
-  else if(joy_data->btn_r1){
-    intake_power = -1.0;
-  }
-  else{
-    intake_power = 0.0;
-  }
-  
-  rhi_ptr_->setMotorVoltageCommandPercent(motor_list[5], intake_power);
-  rhi_ptr_->setMotorVoltageCommandPercent(motor_list[6], intake_power);
+    // rhi_ptr_->setMotorVoltageCommandPercent(motor_list[5], intake_power);
+    // rhi_ptr_->setMotorVoltageCommandPercent(motor_list[6], intake_power);
 
     static bool forklift_pressed = false;
     static bool forklift_up = false;
@@ -520,45 +373,13 @@ void TankRobotPlugin::teleop(double current_time)
       forklift_pressed = false;
     }
 
-  m_digital_io[1] = forklift_up; // forklift
-  m_digital_io[2] = joy_data->btn_l2; // pooper
-  rhi_ptr_->setDigitalIO(m_digital_io);
+    m_digital_io[1] = forklift_up; // forklift
+    m_digital_io[2] = joy_data->btn_l2; // pooper
+    rhi_ptr_->setDigitalIO(m_digital_io);
+
+    // updateDrivetrainMotors();
+  }
 }
-
-// TODO: should/can this also reset ekf?
-void TankRobotPlugin::resetPose(double x, double y, double theta)
-{
-  std::cout << "Resetting Pose!" << std::endl;
-  m_last_odom_pose = m_curr_odom_pose;
-
-  m_init_world_x = x;
-  m_init_world_y = y;
-  m_init_world_theta = theta;
-
-  geometry_msgs::msg::PoseWithCovarianceStamped msg{};
-
-  msg.header.frame_id = "odom";
-  msg.header.stamp = node_ptr_->get_clock()->now();
-
-  msg.pose.pose.position.x = x;
-  msg.pose.pose.position.y = y;
-  msg.pose.pose.position.z = 0;
-
-  ghost_util::yawToQuaternionRad(
-    theta,
-    msg.pose.pose.orientation.w,
-    msg.pose.pose.orientation.x,
-    msg.pose.pose.orientation.y,
-    msg.pose.pose.orientation.z);
-
-  msg.pose.covariance[0] = m_init_sigma_x * m_init_sigma_x;
-  msg.pose.covariance[7] = m_init_sigma_y * m_init_sigma_y;
-  msg.pose.covariance[35] = m_init_sigma_theta * m_init_sigma_theta;
-
-  m_set_pose_publisher->publish(msg);
-}
-
-void TankRobotPLugin::moveToPose(const double x){}
 
 void TankRobotPlugin::worldOdometryUpdateCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
