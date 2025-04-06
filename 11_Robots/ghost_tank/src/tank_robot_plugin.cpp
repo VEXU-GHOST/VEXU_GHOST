3208374c5fe6f69464ac319cd4e64f5bb9ca93ad
Annie <Annie.2005@outlook.com>
2025-04-06 16:57:53 -0500

    testing on robot pls

--- 11_Robots/ghost_tank/src/tank_robot_plugin.cpp
+++ 11_Robots/ghost_tank/src/tank_robot_plugin.cpp
@@ -32,6 +32,116 @@ using ghost_planners::RobotTrajectory;
 using ghost_ros_interfaces::msg_helpers::fromROSMsg;
 using std::placeholders::_1;
 
+namespace ghost_example_robot
+{
+
+GhostExampleRobot::GhostExampleRobot()
+{
+}
+
+void GhostExampleRobot::initialize()
+{
+  // Only called once when program starts!
+  std::cout << "initialize" << std::endl;
+}
+
+void GhostExampleRobot::onNewSensorData()
+{
+  // Optional, called before disabled/autonomous/teleop when new data arrives.
+  // std::cout << "onNewSensorData" << std::endl;
+}
+
+void GhostExampleRobot::disabled()
+{
+  std::cout << "disabled" << std::endl;
+}
+
+void GhostExampleRobot::autonomous(double current_time)
+{
+  std::cout << "Autonomous" << current_time << std::endl;
+}
+
+void GhostExampleRobot::teleop(double current_time)
+{
+  static int loop_count = 0;
+  if (loop_count++ % 100 == 0) {
+    std::cout << "Teleop " << current_time << std::endl;
+  }
+
+  auto joy_data = rhi_ptr_->getMainJoystickData();
+
+  if (joy_data->btn_a) {
+    std::cout << "Button A!" << std::endl;
+  } else if (joy_data->btn_b) {
+    std::cout << "Button B!" << std::endl;
+  } else if (joy_data->btn_x) {
+    std::cout << "Button X!" << std::endl;
+  } else if (joy_data->btn_y) {
+    std::cout << "Button Y!" << std::endl;
+  } else if (joy_data->btn_u) {
+    std::cout << "Button U!" << std::endl;
+  } else if (joy_data->btn_d) {
+    std::cout << "Button D!" << std::endl;
+  } else if (joy_data->btn_l) {
+    std::cout << "Button L!" << std::endl;
+  } else if (joy_data->btn_r) {
+    std::cout << "Button R!" << std::endl;
+  } else if (joy_data->btn_l1) {
+    std::cout << "Button L1!" << std::endl;
+  } else if (joy_data->btn_l2) {
+    std::cout << "Button L2!" << std::endl;
+  }
+
+  // Print joystick data!
+  if (joy_data->btn_r1) {
+    // Left joystick up-down axis is "left_y", left-right axis is "left_x"
+    // Right joystick up-down axis is "right_y", left-right axis is "right_x"
+    std::cout << "Left X: " << joy_data->left_x << std::endl;
+    std::cout << "Left Y: " << joy_data->left_y << std::endl;
+    std::cout << "Right X: " << joy_data->right_x << std::endl;
+    std::cout << "Right Y: " << joy_data->right_y << std::endl;
+    std::cout << std::endl;
+  }
+
+  // While holding button R2, send motor commands based on joystick values
+  if (joy_data->btn_r2) {
+    // Joysticks go from -127 to 127, but motors take a value from -1.0 to 1.0.
+    double left_wheel_power = joy_data->left_y / 127.0;
+    double right_wheel_power = joy_data->right_y / 127.0;
+
+    // setMotorVoltageCommandPercent maps -1.0 <-> 1.0 to -12000 <-> 12000 milliVolts behind the scenes.
+    rhi_ptr_->setMotorVoltageCommandPercent("left_motor", left_wheel_power);
+    rhi_ptr_->setMotorVoltageCommandPercent("right_motor", right_wheel_power);
+
+    // Each motor has a current limit that defaults to zero.
+    // This is so we can carefully allocate battery power between systems.
+    // If we don't set these, the motors will be extremely weak, if they move at all.
+    rhi_ptr_->setMotorCurrentLimitMilliAmps("left_motor", 2500.0);
+    rhi_ptr_->setMotorCurrentLimitMilliAmps("right_motor", 2500.0);
+
+    // Now we can get motor data and print it.
+    double left_position = rhi_ptr_->getMotorPosition("left_motor");
+    double right_position = rhi_ptr_->getMotorPosition("right_motor");
+
+    // These are in degrees. Units and other data can be configured in example_hardware_config.yaml.
+    std::cout << "Left Motor: " << left_position << " deg" << std::endl;
+    std::cout << "Right Motor: " << right_position << " deg" << std::endl;
+    std::cout << std::endl;
+  } else {
+    // Don't forget to turn motors off!
+    rhi_ptr_->setMotorVoltageCommandPercent("left_motor", 0.0);
+    rhi_ptr_->setMotorVoltageCommandPercent("right_motor", 0.0);
+
+    rhi_ptr_->setMotorCurrentLimitMilliAmps("left_motor", 0.0);
+    rhi_ptr_->setMotorCurrentLimitMilliAmps("right_motor", 0.0);
+  }
+}
+} // namespace ghost_example_robot
+
+PLUGINLIB_EXPORT_CLASS(
+  ghost_example_robot::GhostExampleRobot,
+  ghost_ros_interfaces::V5RobotBase)
+
 namespace ghost_tank
 {
 
