4f67fd27942603c0290f5a8029b3190f99b7ac91
Omega Jerry <OmegaJerry@ras.ece.utexas.edu>
2025-05-02 21:13:37 -0500

    adds check for robot name for alpha jerry

--- scripts/launch_hardware.sh
+++ scripts/launch_hardware.sh
@@ -7,7 +7,13 @@ export VEXU_HOME="/home/ghost/VEXU_GHOST"
 source "$VEXU_HOME/scripts/setup_env.sh"
 
 # log to stdout AND /var/log/syslog
-ros2 launch ghost_high_stakes hardware.launch.py 2>&1 | tee /dev/tty |& logger
+if [ -z ${ROBOT_NAME+x} ]; 
+then 
+    echo "var is unset"; 
+    ros2 launch ghost_high_stakes hardware.launch.py 2>&1 | tee /dev/tty |& logger;
+else echo "var is set to '$var'";
+    ros2 launch ghost_high_stakes alpha_jerry.launch.py 2>&1 | tee /dev/tty |& logger;
+fi
 
 logger "RUNNING ros2 launch ghost_high_stakes DONE"
 logger ${PIPESTATUS}
