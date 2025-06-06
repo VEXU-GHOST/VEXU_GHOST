d1bb8934ec6bc4aaa8428d51aa49520103c2fe74
Omega Jerry <OmegaJerry@ras.ece.utexas.edu>
2025-06-06 04:38:09 -0500

    tested interaction

--- 11_Robots/ghost_high_stakes/launch/alpha/alpha.launch.py
+++ 11_Robots/ghost_high_stakes/launch/alpha/alpha.launch.py
@@ -148,10 +148,10 @@ def generate_launch_description():
         odom_ekf_node,
         ekf_pf_node,
         map_ekf_node,
-        color_sensor_intake,
-        color_classifier_intake,
+        # color_sensor_intake,
+        # color_classifier_intake,
         color_sensor_goal_rush_l,
-                #color_sensor_goal_rush_r,
+        # color_sensor_goal_rush_r,
         competition_state_machine_node,
         gpio_expander,
     ])
