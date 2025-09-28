fad6f249ae64859d27cd7300f264b149291f1ac6
Catherine <licatherine0531@gmail.com>
2025-09-28 15:55:45 -0500

    onboarding-III first commit

---
+++ 10_Examples/CatherineLi_onboarding/src/hello_world_tester.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+  //Car myObj; // Create an object of Car
+  //cout << myObj.speed(200); // Call the method with an argument
+  return 0;
+}
+
--- 10_Examples/ghost_example_robot/src/ghost_example_robot.cpp
+++ 10_Examples/ghost_example_robot/src/ghost_example_robot.cpp
@@ -102,7 +102,8 @@ void GhostExampleRobot::teleop(double current_time)
 
   // While holding button R2, send motor commands based on joystick values
   if (joy_data->btn_r2) {
-    // Joysticks go from -127 to 127, but motors take a value from -1.0 to 1.0.
+
+    /*// Joysticks go from -127 to 127, but motors take a value from -1.0 to 1.0.
     double left_wheel_power = joy_data->left_y / 127.0;
     double right_wheel_power = joy_data->right_y / 127.0;
 
@@ -123,7 +124,18 @@ void GhostExampleRobot::teleop(double current_time)
     // These are in degrees. Units and other data can be configured in example_hardware_config.yaml.
     std::cout << "Left Motor: " << left_position << " deg" << std::endl;
     std::cout << "Right Motor: " << right_position << " deg" << std::endl;
-    std::cout << std::endl;
+    std::cout << std::endl;*/
+
+    auto joy_data = rhi_ptr_->getMainJoystickData();
+
+    double forward_vel = joy_data->left_y / 127.0;
+    double angular_vel = joy_data->right_x / 127.0;
+
+    double threshold = 0.05;
+    forward_vel = (std::fabs(forward_vel) < threshold) ? 0.0 : forward_vel;
+    angular_vel = (std::fabs(angular_vel) < threshold) ? 0.0 : angular_vel;
+
+
   } else {
     // Don't forget to turn motors off!
     rhi_ptr_->setMotorVoltageCommandPercent("left_motor", 0.0);
