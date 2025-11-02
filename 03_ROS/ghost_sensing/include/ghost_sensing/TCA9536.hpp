4ffc988b3d19158a9dae64eb3d22bc425499777c
Barrett <barrettlubianski@gmail.com>
2025-11-02 17:59:17 -0600

    Tested prototype

--- 03_ROS/ghost_sensing/include/ghost_sensing/TCA9536.hpp
+++ 03_ROS/ghost_sensing/include/ghost_sensing/TCA9536.hpp
@@ -4,18 +4,19 @@
 #include <string>
 #include <memory>
 #include <vector>
-#include "driver_tcs34725_interface.h"
+#include <linux_i2c_interface.h>
 
 namespace ghost_sensing {
 
 class TCA9536 {
 public:
 
-    TCA9536(uint8_t I2c_address);
+    TCA9536(uint8_t I2c_address, std::vector<uint8_t> initial_input_states);
     void write_register(uint8_t reg, uint8_t value);
     uint8_t read_register(uint8_t reg);
 private:
     std::shared_ptr<linux_i2c_interface> m_i2c_communication;
     uint8_t I2c_address;
+    std::vector<uint8_t> inputs;
 };
 }
