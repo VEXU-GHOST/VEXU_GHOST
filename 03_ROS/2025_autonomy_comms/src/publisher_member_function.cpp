49c0ed9beb700ecf9b380147c72f5d3a3478758e
Uniostar <akvkmg2007@gmail.com>
2025-11-16 15:33:16 -0600

    11/16 aryan worker package

--- 03_ROS/2025_autonomy_comms/src/publisher_member_function.cpp
+++ 03_ROS/2025_autonomy_comms/src/publisher_member_function.cpp
@@ -67,15 +67,16 @@ using namespace vex;
 
 // Instance of message link class
 vex::message_link LinkA( PORT11, "vex_robotics_team_1234_A", linkType::manager );
+
 // Task to periodically send test messages to worker robot
-int
-sendTask() {
+int sendTask() 
+{
   // wait for link
-  while(!LinkA.isLinked())
-    this_thread::sleep_for(50);
+  while(!LinkA.isLinked()) this_thread::sleep_for(50);
   
   // send demo messages
-  while(1) {
+  while(1)
+  {
     LinkA.send("drive");
     this_thread::sleep_for(500);
     LinkA.send("go_forward", 100 );
@@ -88,13 +89,16 @@ sendTask() {
 }
 
 
-int main() {
+int main() 
+{
   // start demo task
   thread t1(sendTask);
 
   // show link status
-  while(1) {
+  while(1) 
+  {
     Brain.Screen.printAt(10, 50, true, "Link: %s", LinkA.isLinked() ? "ok" : "--" );
+    
     // Allow other tasks to run
     this_thread::sleep_for(10);
   }
