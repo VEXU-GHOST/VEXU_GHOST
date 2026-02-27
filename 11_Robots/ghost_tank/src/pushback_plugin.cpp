4fc540e7f45fa7b697cb390402da18b1ba810a6b
jennyc518 <yingchen.chen@utexas.edu>
2026-02-26 18:23:51 -0600

    template

---
+++ 11_Robots/ghost_tank/src/pushback_plugin.cpp
@@ -0,0 +1,175 @@
+/*
+ *   Copyright (c) 2024 Maxx Wilson
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+#include <ghost_tank/tank_model.hpp>
+#include <ghost_tank/pushback_plugin.hpp>
+#include <ghost_util/angle_util.hpp>
+#include <ghost_util/math_util.hpp>
+#include <ghost_util/unit_conversion_utils.hpp>
+#include <pluginlib/class_list_macros.hpp>
+#include <ghost_util/read_path.hpp>
+#include <ghost_control/models/v5_current_limiting.hpp>
+
+using ghost_planners::RobotTrajectory;
+using ghost_ros_interfaces::msg_helpers::fromROSMsg;
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+using ghost_v5_interfaces::devices::JoystickDeviceData;
+
+using ghost_util::INCHES_TO_METERS;
+
+using JoyPtr = std::shared_ptr<ghost_v5_interfaces::devices::JoystickDeviceData>;
+
+namespace ghost_tank
+{
+
+PushbackPlugin::PushbackPlugin()
+{
+  std::cout << "PushbackPlugin::PushbackPlugin" << std::endl;
+  populateMotorNames();
+  populateDigitalIONames();
+}
+
+
+// TODO: Change motor names
+void PushbackPlugin::populateMotorNames()
+{
+  m_right_drive_motor_names = {
+    "drive_r1",
+    "drive_r2",
+    "drive_r3",
+    "drive_r4",
+    "drive_r5",
+    "drive_r6",
+    "drive_r7",
+    "drive_r8",
+  };
+  m_left_drive_motor_names = {
+    "drive_l1",
+    "drive_l2",
+    "drive_l3",
+    "drive_l4",
+    "drive_l5",
+    "drive_l6",
+    "drive_l7",
+    "drive_l8",
+  };
+
+  m_all_drive_motor_names.insert(
+    m_all_drive_motor_names.end(),
+    m_left_drive_motor_names.begin(),
+    m_left_drive_motor_names.end());
+
+  m_all_drive_motor_names.insert(
+    m_all_drive_motor_names.end(),
+    m_right_drive_motor_names.begin(),
+    m_right_drive_motor_names.end());
+}
+
+
+// TODO: Change pneumatic names
+void PushbackPlugin::populateDigitalIONames()
+{
+  digital_io_port_map["goal_rush_l"] = 0;
+  digital_io_port_map["climb"] = 1;
+  digital_io_port_map["goal_rush_r"] = 2;
+  digital_io_port_map["bite"] = 3;
+  digital_io_port_map["clamp"] = 4;
+  digital_io_port_map["shooter"] = 5;
+}
+
+//////////////////////
+/// Initialization ///
+//////////////////////
+
+void PushbackPlugin::initialize()
+{
+  std::cout << "PushbackPlugin::initialize" << std::endl;
+  TankRobotPlugin::initialize();
+  // TankRobotPlugin::initROSComms();
+  // TankRobotPlugin::initEstimation();
+  // TankRobotPlugin::initIntake();
+  // TankRobotPlugin::initTankModel();
+  // TankRobotPlugin::initAutonomy();
+  // TankRobotPlugin::resetWorldPose();
+}
+
+void PushbackPlugin::disabled()
+{
+}
+
+void PushbackPlugin::autonomous(double current_time)
+{
+  if (m_is_first_auton_loop) {
+    // m_is_first_auton_loop = false;
+    playTTS("starting autonomous");
+    // m_odom_ptr->resetPose();
+    // resetWorldPose();
+
+    bt_->set_variable<bool>("clamp_closed", false);
+    bt_->set_variable<bool>("bite_closed", false);
+    bt_->set_variable<bool>("goal_rush_l_down", false);
+    bt_->set_variable<bool>("goal_rush_r_down", false);
+    bt_->set_variable<bool>("goal_rush_down", false);
+    bt_->set_variable<bool>("climb_extended", false);
+    bt_->set_variable<bool>("conveyor_active", false);
+    bt_->set_variable<bool>("store_ring", false);
+    bt_->set_variable<bool>("ring_detector_active", false);
+    bt_->set_variable<bool>("shoot", false);
+  }
+
+  TankRobotPlugin::autonomous(current_time);
+
+  // Update Pneumatics
+  rhi_ptr_->setDigitalOut(digital_io_port_map["climb"], (bt_->get_variable<bool>("climb_extended")));
+  rhi_ptr_->setDigitalOut(digital_io_port_map["clamp"], (bt_->get_variable<bool>("clamp_closed")));
+  rhi_ptr_->setDigitalOut(digital_io_port_map["shooter"], (bt_->get_variable<bool>("shoot")));
+  if (m_mirrored) {
+    rhi_ptr_->setDigitalOut(digital_io_port_map["goal_rush_l"], bt_->get_variable<int>("goal_rush_r_down") || bt_->get_variable<int>("goal_rush_down"));
+    rhi_ptr_->setDigitalOut(digital_io_port_map["goal_rush_r"], bt_->get_variable<int>("goal_rush_l_down"));
+  } else {
+    rhi_ptr_->setDigitalOut(digital_io_port_map["goal_rush_l"], (bt_->get_variable<int>("goal_rush_l_down")));
+    rhi_ptr_->setDigitalOut(digital_io_port_map["goal_rush_r"], (bt_->get_variable<int>("goal_rush_r_down") || bt_->get_variable<int>("goal_rush_down")));
+  }
+  rhi_ptr_->setDigitalOut(digital_io_port_map["bite"], m_bite_closed);
+}
+
+void PushbackPlugin::teleop(double current_time)
+{
+  auto joy_data = rhi_ptr_->getMainJoystickData();
+  bool shift_r = joy_data->btn_b;
+  bool shift_l = joy_data->btn_d;
+
+  TankRobotPlugin::teleop(current_time);
+
+  // updateScissor(joy_data->btn_l1, joy_data->btn_l2, shift_r);
+  // updateT1Climb(joy_data->btn_r1, joy_data->btn_r2, shift_r);
+}
+
+} // namespace ghost_tank
+
+PLUGINLIB_EXPORT_CLASS(ghost_tank::PushbackPlugin, ghost_ros_interfaces::V5RobotBase)
