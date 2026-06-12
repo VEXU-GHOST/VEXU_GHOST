1e9c6be10952f2ef300a4e9731e403ff62a9a89d
Alpha Jerry <alphajerry@ras.ece.utexas.edu>
2026-06-12 12:47:25 -0500

    specific package clean

--- scripts/build/clean.sh
+++ scripts/build/clean.sh
@@ -9,6 +9,18 @@ fi
 
 cd $VEXU_HOME
 
+# If package names are given, clean only those packages' artifacts and exit.
+if [ "$#" -gt 0 ];
+then
+    for pkg in "$@";
+    do
+        echo "Cleaning $pkg artifacts..."
+        [ -d "build/$pkg" ] && sudo rm -r "build/$pkg"
+        [ -d "install/$pkg" ] && sudo rm -r "install/$pkg"
+    done
+    exit 0
+fi
+
 [ -d "build" ] && sudo rm -r build/
 [ -d "install" ] && sudo rm -r install/
 [ -d "log" ] && sudo rm -r log/
